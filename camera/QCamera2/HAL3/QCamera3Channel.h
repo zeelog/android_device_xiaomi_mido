f01cfe26b06b044fd7e73f4b68e8178c36222ed1
Shukla Mradul <mradul_shukla@codeaurora.org>
2017-05-05 12:09:11 +0530

    mido: camera: HAL3: Error occurs while unmapping buffer
    Issue:
    STL operation such as erase ,push_back needs to be atomic.
    
    List to keep reprocessed meta and buffer is prone to
    simultaneous access from other part of codes causing STL
    operations to fail.
    
    Fix:
    Keeping STL operation of list for buffer and meta both in mutex.
    
    Change-Id: I8b966ee993a981ae64099093ff131d305f0cca94

--- camera/QCamera2/HAL3/QCamera3Channel.h
+++ camera/QCamera2/HAL3/QCamera3Channel.h
@@ -569,6 +569,8 @@ private:
     int32_t resetToCamPerfNormal(uint32_t frameNumber);
     android::List<OfflineBuffer> mOfflineBuffers;
     android::List<OfflineBuffer> mOfflineMetaBuffers;
+    Mutex mOfflineBuffersLock;
+    Mutex mOfflineMetaBuffersLock;
     int32_t mOfflineBuffersIndex;
     int32_t mOfflineMetaIndex;
     uint32_t mFrameLen;
