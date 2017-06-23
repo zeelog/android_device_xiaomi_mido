83391ea85d14b7c9d19006193364144c2e3a28f9
Jean-Michel Trivi <jmtrivi@google.com>
2017-06-22 19:20:26 -0700

    mido: Media volume: from 15 to 25 steps
    Test: play media, use all 25 vol steps and observe varying attenuation
    Bug: 32195657
    Change-Id: I7ac87df0b2b15de3a7bb87e739bd88ef7b185236

--- prop.mk
+++ prop.mk
@@ -15,6 +15,7 @@ persist.vendor.audio.fluence.voicerec=true \
 persist.vendor.audio.hw.binder.size_kbyte=1024 \
 persist.vendor.btstack.enable.splita2dp=false \
 ro.af.client_heap_size_kbyte=7168 \
+ro.config.media_vol_steps=25 \
 ro.config.vc_call_vol_steps=7 \
 ro.vendor.audio.sdk.fluencetype=fluence \
 ro.vendor.audio.sdk.ssr=false \
