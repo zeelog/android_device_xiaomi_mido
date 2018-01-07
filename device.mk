82410e67a393821dd3ab76915cd51c82d509b148
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-07 14:01:18 -0800

    mido: Add QTI privileged app permissions
    Change-Id: Ide1b79d83624e8aba2ee17979efacc356dfacb55

--- device.mk
+++ device.mk
@@ -286,7 +286,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
 # QMI
 PRODUCT_PACKAGES += \
