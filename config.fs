7fb7034fb7612006707a3315f3de5f8bea752759
Devi Sandeep Endluri V V <dendluri@codeaurora.org>
2018-01-25 20:43:35 +0000

    mido: config.fs: Added wake_alarm capabiltiy
    - imsrcsd starts alarm timers hence needs
      WAKE_ALARM capability.
    
    Change-Id: I0e72998bab6b17ca6222e5a931d26275fa784e97

--- config.fs
+++ config.fs
@@ -91,6 +91,18 @@ user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
+[vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: WAKE_ALARM
+
+[system/vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: WAKE_ALARM
+
 [vendor/bin/cnd]
 mode: 0755
 user: AID_SYSTEM
