21b389c481ec2104af4b795357bb644cb41a2b73
Rajeev Kumar <rajekumar@google.com>
2018-03-12 15:26:29 -0700

    LMKD: Enable memory stats logging in user mode LMKD
    Bug:74443701
    Test: Tested manually
    Change-Id: I288098e17d5fc65923758152bba5e786a75edc4c

--- device.mk
+++ device.mk
@@ -280,6 +280,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service.xiaomi_mido
 
+# LMKD
+# Enable stats logging in LMKD
+TARGET_LMKD_STATS_LOG := true
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lmk.log_stats=true
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
