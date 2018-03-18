abdc246f1551a47a1b403902e9f37e7c52c8ab8b
Bruno Martins <bgcngm@gmail.com>
2018-03-18 21:20:20 +0000

    mido: Remove non-existing HAL services
    Change-Id: I26da013dfa359ec8f184a20bc881558be0144b4a

--- device.mk
+++ device.mk
@@ -83,7 +83,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.primary.msm8953 \
