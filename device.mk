2a589d1ab890b713d994d08972d492f7cb225e35
TheScarastic <warabhishek@gmail.com>
2018-03-22 23:37:14 +0530

    mido: Import xiaomidoze from xiaomi/msm8996
    Change-Id: Ie71ec026a885fc2df72c52c64b1a9c7e7e04fecf

--- device.mk
+++ device.mk
@@ -163,6 +163,10 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.0 \
     vendor.display.config@1.0_vendor
 
+# Doze mode
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
