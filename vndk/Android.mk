8f4e1915a35beecad865574002013b2506511bb4
TheScarastic <warabhishek@gmail.com>
2018-03-24 15:12:25 +0530

    mido: vendor.lineage.power@1.0 from vndk
     * https://github.com/LineageOS/android_hardware_qcom_power/commit/d3eb25f686ea4b4ee157fd63cae9fd023408657c
    
    Change-Id: Ic614fcfa2de24507b4020a498e1ce9042308d3d6

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -32,7 +32,6 @@ EXTRA_VENDOR_LIBRARIES := \
     android.hidl.manager@1.0 \
     vendor.display.color@1.0 \
     vendor.display.config@1.0 \
-    vendor.lineage.power@1.0 \
     vendor.qti.hardware.iop@1.0
 
 
