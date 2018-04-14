114907a5788b1f48f4903eccdfa75a3bc0c98c16
Hua Liu <richardl@codeaurora.org>
2018-04-13 19:19:09 -0700

    mido: Fix MBN copy issue
    DAC_OVERRIDE is disabled for all the vendor init scripts
    in Android P. This change will grant "group root" the write
    permission temporarily for deleting/copying operations.
    
    System user could not access the MBN files in data partition
    no longer. Only radio user and root group have read permission
    after boot up.
    
    Change-Id: I9f184da2253dc5896b145c0e52fc3b42da15d9f7

--- rootdir/init.qcom.sh
+++ rootdir/init.qcom.sh
@@ -354,13 +354,17 @@ fi
 
 cur_version_info=`cat /firmware/verinfo/ver_info.txt`
 if [ ! -f /firmware/verinfo/ver_info.txt -o "$prev_version_info" != "$cur_version_info" ]; then
+    # add W for group recursively before delete
+    chmod g+w -R /data/vendor/modem_config/*
     rm -rf /data/vendor/modem_config/*
     # preserve the read only mode for all subdir and files
     cp --preserve=m -dr /firmware/image/modem_pr/mcfg/configs/* /data/vendor/modem_config
     cp --preserve=m -d /firmware/verinfo/ver_info.txt /data/vendor/modem_config/
     cp --preserve=m -d /firmware/image/modem_pr/mbn_ota.txt /data/vendor/modem_config/
-    chown -hR radio.radio /data/vendor/modem_config/*
+    # the group must be root, otherwise this script could not add "W" for group recursively
+    chown -hR radio.root /data/vendor/modem_config/*
 fi
+chmod g-w /data/vendor/modem_config
 setprop ro.runtime.mbn_copy_completed 1
 
 #check build variant for printk logging
