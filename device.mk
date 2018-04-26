203e4c3879fe7f035456fabf89cdb9ffca7d8187
Bruno Martins <bgcngm@gmail.com>
2018-04-27 00:04:21 +0100

    mido: List additional native libraries to expose to apps
     * Initially based on the list defined by Xiaomi, trimmed to include
       only the vendor libs that are also being shipped by us.
    
     * Example bellow of a failure before one lib was made accessible to
       a camera app.
    
       Before exposing:
         I CAM_HexagonEnv: Will attempt to load libadsprpc.so from the system partition
         I CAM_HexagonEnv: Loading libhalide_hexagon_host.so from the app
         I CAM_CamBoxHelper: Computing layout for window: 1080x1920, and preview: 3000x4000, orientation: PORTRAIT, isMultiWindow: false
         I CAM_CamBoxHelper: placing extra widgets above the bottom bar
         I CAM_CamBoxHelper: Computed layout: CameraLayoutBoxes{window=1080x1920, preview=Rect(0, 106 - 1080, 1546), uncoveredPreview=Rect(0, 173 - 1080, 1546), topBar=Rect(0, 0 - 1080, 106), optionsBar=Rect(0, 0 - 1080, 173), bottomBar=Rect(0, 1546 - 1080, 1920), extraBottomWidgets=Rect(0, 1488 - 1080, 1546)}
         W CAM_HexagonEnv: Failed to load Hexagon library: java.lang.UnsatisfiedLinkError: dlopen failed: library "libadsprpc.so" not found
    
       After exposing:
         I CAM_HexagonEnv: Will attempt to load libadsprpc.so from the system partition
         I CAM_HexagonEnv: Loading libhalide_hexagon_host.so from the app
         I CAM_CamBoxHelper: Computed layout: CameraLayoutBoxes{window=1078x1860, preview=Rect(0, 0 - 1078, 808), uncoveredPreview=Rect(0, 173 - 1078, 1437), topBar=Rect(0, 0 - 1078, 106), optionsBar=Rect(0, 0 - 1078, 173), bottomBar=Rect(0, 1437 - 1078, 1860), extraBottomWidgets=Rect(0, 1437 - 1078, 1495)}
         I CAM_HexagonEnv: Writing libhalide_hexagon_remote_skel.so to /data/user/0/<package_name_removed>/libhalide_hexagon_remote_skel.so
         I CAM_HexagonEnv: ADSP_LIBRARY_PATH=/data/user/0/<package_name_removed>;/dsp
    
    Change-Id: I552dc44a32221d2d3cec48e1c64ed72c470d412e

--- device.mk
+++ device.mk
@@ -73,6 +73,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Additional native libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # ANT
 PRODUCT_PACKAGES += \
     AntHalService \
