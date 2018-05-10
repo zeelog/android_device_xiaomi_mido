6bba0742b0a741419fdd9668c9a34753f6e6c4c3
Muhammed Siju <msiju@codeaurora.org>
2018-05-10 18:13:07 +0530

    mido: Enable Camera1 API for legacy targets.
    Set persist.vendor.qti.telephony.vt_cam_interface=1 for
    legacy targets to enable Camera1 API usage in VT telephony.
    
    Change-Id: Ia41c6c96688532519877b61badcdcc3cf7c5bb9a
    CRs-Fixed: 2076679

--- prop.mk
+++ prop.mk
@@ -52,6 +52,7 @@ media.camera.ts.monotonic=1 \
 persist.camera.gyro.disable=0 \
 persist.camera.isp.clock.optmz=0 \
 persist.camera.stats.test=5 \
+persist.vendor.qti.telephony.vt_cam_interface=1 \
 vidc.enc.dcvs.extra-buff-count=2
 
 # Cne/Dpm
