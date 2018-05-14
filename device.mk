16a58235e516acb2b8ed65459cb493fae7cbb2ed
Michael Bestas <mkbestas@lineageos.org>
2018-05-14 22:15:24 +0300

    mido: Don't build tinymix
    * This tool can be built manually for debugging cases,
      there is no need to ship it in production
    
    Change-Id: I942a32c33b07c790e1a3cc326ada4b4bb448576e

--- device.mk
+++ device.mk
@@ -99,8 +99,7 @@ PRODUCT_PACKAGES += \
     libaudio-resampler \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libqcompostprocbundle \
-    tinymix
+    libqcompostprocbundle
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
