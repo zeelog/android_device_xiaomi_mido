582d512cf6a4a5e452cc696d2f71a353b8aab36c
TheScarastic <warabhishek@gmail.com>
2018-05-22 14:31:28 +0530

    mido: Generate thermal-engine.conf
     * adb shell thermal-engine –o > thermal-engine.conf
    
    Change-Id: If934f33d43c7d74008e63ceef8dd03d420c624dc

--- device.mk
+++ device.mk
@@ -332,6 +332,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
