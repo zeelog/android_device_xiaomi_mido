4299dca95191015729ca3516260ef85b2a25b63e
Michael Bestas <mkbestas@lineageos.org>
2018-05-26 00:49:11 +0300

    mido: Add powerhint.xml
     * Based on LA.UM.6.6.r1-08500-89xx.0, adapted for the new format.
     * Required by perf_hint in libqti-perfd-client.so.
    
    Change-Id: Ief38dc242a92de05305452634fe2318e4de42f90

--- device.mk
+++ device.mk
@@ -286,6 +286,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-service-qti
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # Properties
 -include device/xiaomi/mido/prop.mk
 
