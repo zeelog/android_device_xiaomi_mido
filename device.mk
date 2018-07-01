936c482749e8ae8961d6b25290ad31efee63918e
LuK1337 <priv.luk@gmail.com>
2018-07-01 14:41:43 +0200

    mido: Remove unnecessary languages_full.mk include
    * It's already included in full_base_telephony.
    
    Change-Id: I7547ff447e81641e74fd911a12f5a9f0df9e5ccf

--- device.mk
+++ device.mk
@@ -14,8 +14,6 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 $(call inherit-product, vendor/xiaomi/mido/mido-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n.mk)
 
@@ -28,7 +26,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
     Settings \
     SettingsProvider \
     SystemUI \
-    framework-res \
+    framework-res
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
