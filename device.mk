55c29483b5ba6a17f027ca243bbd1568ac6a350b
TheScarastic <warabhishek@gmail.com>
2018-07-03 00:42:30 +0530

    mido: recovery: Add recovery scripts
     * Add bootdevice symlink
     * Advertise system_as_root
     * Support usb functions

--- device.mk
+++ device.mk
@@ -328,6 +328,8 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
+    init.recovery.qcom.rc \
+    init.recovery.qcom.usb.rc \
     init.target.rc \
     ueventd.qcom.rc
 
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -35,6 +35,22 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.qcom.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.qcom.usb.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.recovery.qcom.usb.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.target.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
