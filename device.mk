83feb713a34a4d159321e33515620c5bea0837bd
Bruno Martins <bgcngm@gmail.com>
2018-07-07 22:40:59 +0100

    mido: Remove implicitly built QTI camera shared libraries
     * These shared libs are built when TARGET_USES_QTI_CAMERA_DEVICE
       flag is enabled.
    
    Change-Id: Ifb39b7c2c8de2cbb1f59c6dc492a3763d32f1b7a

--- device.mk
+++ device.mk
@@ -119,9 +119,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
     libmm-qcamera \
-    Snap \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    Snap
 
 # Configstore
 PRODUCT_PACKAGES += \
