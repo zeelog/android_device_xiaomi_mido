45fb1b50d19895804354678d44e764f9f767ea96
ZeeLog <Danjel.Zammit@gmail.com>
2018-07-09 13:09:15 +0200

    mido: AID/fs: further cleanup
    slim_daemon has been dropped
    
    Change-Id: I4e92402b1e8bf802f3a04a1717553a7a964d6caf

--- config.fs
+++ config.fs
@@ -55,12 +55,6 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
 
-[vendor/bin/slim_daemon]
-mode: 0755
-user:  AID_GPS
-group: AID_GPS
-caps: NET_BIND_SERVICE
-
 [vendor/bin/xtwifi-client]
 mode: 0755
 user:  AID_GPS
