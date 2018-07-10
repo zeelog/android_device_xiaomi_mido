bb769cbb452aaa0aee7cc58d1e623bab9dac2d95
Albert I <krascgq@outlook.co.id>
2018-07-10 16:37:28 +0800

    Revert "mido: Add few packages to PRODUCT_ENFORCE_RRO_TARGETS"
    This reverts commit d8fc43f6a4f7239b6ab9d075ea3dd78462763d02.
    
     Conflicts:
    	msm8953.mk
    
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- device.mk
+++ device.mk
@@ -23,13 +23,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n.mk)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    Bluetooth \
-    Settings \
-    SettingsProvider \
-    SystemUI \
-    framework-res \
-
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
