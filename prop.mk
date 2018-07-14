4d142ce3a06c246321c38aac74ed2f896d2e2887
Albert I <krascgq@outlook.co.id>
2018-07-14 20:10:41 +0800

    mido: Set vendor security patch level
    * Required by Trust
    * Taken from tissot OPR6.170623.026.8.1.10
    
    Change-Id: I383cf9b30d9d526a3cdd26a1ef9b60ca30003b1d
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- prop.mk
+++ prop.mk
@@ -178,6 +178,10 @@ ro.telephony.default_network=22,20 \
 service.qti.ims.enabled=1 \
 telephony.lteOnCdmaDevice=1
 
+# Security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.vendor.build.security_patch=2018-01-01
+
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.timed.enable=true
