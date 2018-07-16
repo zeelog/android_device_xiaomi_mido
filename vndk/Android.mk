1b86f79d0297112199a8b129e973fcc76a4a3c3c
TheScarastic <warabhishek@gmail.com>
2018-07-16 21:15:28 +0530

    mido: Update blobs from Tissot OPM1.171019.019
    Change-Id: I4ccdc508f8f8ff095e14418c70bf3a2b0d1ad082

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -31,8 +31,7 @@ EXTRA_VENDOR_LIBRARIES := \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0 \
     vendor.display.color@1.0 \
-    vendor.display.config@1.0 \
-    vendor.qti.hardware.iop@1.0
+    vendor.display.config@1.0
 
 
 #-------------------------------------------------------------------------------
