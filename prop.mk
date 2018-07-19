c07921eb35ab43fbd571b7f99a859c52a6ad7b39
ZeeLog <Danjel.Zammit@gmail.com>
2018-07-19 08:41:33 +0200

    mido: remove unneeded/unused camera property
    Test: Both Skype & Hangouts video calls work after
          removing this property.
    
    Change-Id: I369ed5bff94217fab1cbcaf6b9cb7abd43095fbb

--- prop.mk
+++ prop.mk
@@ -51,7 +51,6 @@ ro.qualcomm.bt.hci_transport=smd
 PRODUCT_PROPERTY_OVERRIDES += \
 camera.display.lmax=1280x720 \
 camera.display.umax=1920x1080 \
-camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
 camera.lowpower.record.enable=1 \
 media.camera.ts.monotonic=1 \
 persist.camera.gyro.disable=0 \
