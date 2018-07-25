a2c0285118cd81e950b848dba8aab07aa81810d6
Lakshman Chaluvaraju <lchalu@codeaurora.org>
2018-07-25 18:09:15 +0530

    mido: HAL compatibility for P upgrade
    HAL compatibility for P upgrade.
    Include the required product packages.
    
    Change-Id: I4de0c149628c1fd4b6e15786bcf3f6ef909968c9
    CRs-Fixed: 2272748

--- device.mk
+++ device.mk
@@ -78,7 +78,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
+    android.hardware.audio@4.0 \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.common@4.0 \
+    android.hardware.audio.common@4.0-util \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@4.0 \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.0-impl \
     audio.a2dp.default \
     audio.primary.msm8953 \
