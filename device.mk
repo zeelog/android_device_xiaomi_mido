14466050a5a69396bdfb34578fd04bac6668f81e
LuK1337 <priv.luk@gmail.com>
2018-08-12 19:40:00 +0530

    mido: patchelf gnss files
    Change-Id: Ia44f1080bee6ffa6bc4ca461a1f9c57697927fd5

--- device.mk
+++ device.mk
@@ -342,6 +342,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndk-sp
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:system/lib64/android.hardware.gnss@1.0-v27.so
+
 # Thermal
 PRODUCT_PACKAGES += \
     thermal.msm8953
