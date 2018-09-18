132d6247644fd0b7cc4da7e4bc5dcc02d811a95a
Ramjee Singh <ramjee@codeaurora.org>
2018-09-18 12:40:44 +0530

    mido: ST HAL upgrade for P compatibility
    -Sound trigger HIDL interface supports loading of more
    than 1MB sound models from 2.1 version. Update to use HIDL
    version 2.1.
    
    Change-Id: I564f598f41f20a01ba9e8dd69be125097a134f1c

--- device.mk
+++ device.mk
@@ -85,7 +85,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@4.0 \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.primary.msm8953 \
     audio.r_submix.default \
