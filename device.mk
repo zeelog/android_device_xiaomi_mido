8c1fcbdc1a2aff6a566742042421f2fcb42b75a9
Han Wang <416810799@qq.com>
2018-10-02 14:00:56 +0200

    mido: Update textclassifier package name for P
    Change-Id: If846cc590060025202fa336af4420747f6b01238

--- device.mk
+++ device.mk
@@ -328,7 +328,7 @@ PRODUCT_COPY_FILES += \
 
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
-    textclassifier.smartselection.bundle1
+    textclassifier.bundle1
 
 # Thermal
 PRODUCT_COPY_FILES += \
