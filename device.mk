8e53fb2f1d63ce249cd5a5aec316f1fac5eea0d1
Ethan Chen <intervigil@gmail.com>
2018-10-02 13:51:03 +0200

    mido: Use Soong namespace to avoid device build conflicts
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39

--- device.mk
+++ device.mk
@@ -32,6 +32,9 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += device/xiaomi/mido
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
