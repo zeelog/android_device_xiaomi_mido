0be0b603e6e543a480878eadff69ec4747dbb3fb
ZeeLog <Danjel.Zammit@gmail.com>
2018-11-01 20:32:39 +0100

    mido: update build fingerprint to V9.6.3.0.NCFMIFD
    Change-Id: I257bb882faa3468606bf8c5cf7e7c80a66acc611

--- lineage_mido.mk
+++ lineage_mido.mk
@@ -36,7 +36,7 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V9.6.2.0.NCFMIFD release-keys"
+    PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V9.6.3.0.NCFMIFD release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/mido/mido:7.0/NRD90M/V9.6.2.0.NCFMIFD:user/release-keys"
+BUILD_FINGERPRINT := "xiaomi/mido/mido:7.0/NRD90M/V9.6.3.0.NCFMIFD:user/release-keys"
