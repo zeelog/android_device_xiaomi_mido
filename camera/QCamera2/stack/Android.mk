7e2dfd37305eb19acc32f46c67c75c4292254f29
ZeeLog <Danjel.Zammit@gmail.com>
2018-11-01 20:23:07 +0100

    mido: camera cleanup: remove test packages
    Change-Id: I039ac18756ae6174cc7132167799afddebfc0661

--- camera/QCamera2/stack/Android.mk
+++ camera/QCamera2/stack/Android.mk
@@ -1,5 +1,3 @@
 LOCAL_PATH:= $(call my-dir)
 include $(LOCAL_PATH)/mm-camera-interface/Android.mk
 include $(LOCAL_PATH)/mm-jpeg-interface/Android.mk
-include $(LOCAL_PATH)/mm-jpeg-interface/test/Android.mk
-#include $(LOCAL_PATH)/mm-camera-test/Android.mk
