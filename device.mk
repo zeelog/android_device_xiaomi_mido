34471c059d28be58fb863a785055e5ef145101c6
Artem Borisov <dedsa2002@gmail.com>
2018-11-07 12:25:37 +0400

    mido: Enable USB via ConfigFS
    Required on 4.9 now.
    
    Change-Id: I1b4557da497c3c63d64770f94a2ff56e72602f8a

--- device.mk
+++ device.mk
@@ -304,6 +304,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.goodix.sh \
+    init.msm.usb.configfs.rc \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -11,6 +11,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE	   := init.msm.usb.configfs.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.msm.usb.configfs.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
