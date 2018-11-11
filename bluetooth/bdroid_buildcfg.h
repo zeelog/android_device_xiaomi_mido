b7b281003570ed36ad164a1c77690fc59f8d1fc9
Umesh Vats <uvats@codeaurora.org>
2018-11-11 22:48:57 +0100

    mido: Bluetooth: Increase number of SEPs to support SHO/Mcast
    Increased number of SEPs to support SHO/Mcast
    
    Change-Id: I2a40281876a9e0cdbf3d13150662575e0d49df14

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -27,6 +27,9 @@
 #define BT_CLEAN_TURN_ON_DISABLED 1
 #define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
 
+/* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
+#define AVDT_NUM_SEPS 12
+
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define BTM_WBS_INCLUDED TRUE
 #define BTIF_HF_WBS_PREFERRED TRUE
