5b8ae5aa66c92e6a5cda4896263bc6a3e24f1322
ZeeLog <Danjel.Zammit@gmail.com>
2018-11-20 10:19:22 +0100

    mido: config.fs: cleanup redundant entries
    * daemons/services path has been updated to /vendor/bin/
    
    * remove also inexistent cnss_daemon
    
    Change-Id: Iaea669929fb7e34b57d32bf26e220b7a2d144799

--- config.fs
+++ config.fs
@@ -16,43 +16,13 @@ user: AID_BLUETOOTH
 group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND
 
-[system/vendor/bin/wcnss_filter]
-mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
-caps: BLOCK_SUSPEND
-
 [vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
 mode: 0755
 user: AID_BLUETOOTH
 group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND NET_ADMIN
 
-[system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: BLOCK_SUSPEND NET_ADMIN
-
-[system/bin/cnss-daemon]
-mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
-caps: NET_BIND_SERVICE
-
 [vendor/bin/pm-service]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/vendor/bin/pm-service]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
@@ -64,25 +34,7 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/pd-mapper]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/bin/pd-mapper]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
 [vendor/bin/imsdatadaemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/vendor/bin/imsdatadaemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
@@ -94,19 +46,7 @@ user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/ims_rtp_daemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_RADIO
-caps: NET_BIND_SERVICE
-
 [vendor/bin/imsrcsd]
-mode: 0755
-user: AID_SYSTEM
-group: AID_RADIO
-caps: WAKE_ALARM
-
-[system/vendor/bin/imsrcsd]
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
@@ -118,19 +58,7 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
 
-[system/vendor/bin/cnd]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
-
 [vendor/bin/slim_daemon]
-mode: 0755
-user:  AID_GPS
-group: AID_GPS
-caps: NET_BIND_SERVICE
-
-[system/vendor/bin/slim_daemon]
 mode: 0755
 user:  AID_GPS
 group: AID_GPS
