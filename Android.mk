33eb045f2ccbd39ff66dff6cc897a326e5ffce06
dianlujitao <dianlujitao@lineageos.org>
2018-12-04 01:11:51 +0100

    mido: Kang ims.apk and qti-telephony-common.jar from daisy
     * From: xiaomi/daisy/daisy_sprout:9/PKQ1.180917.001/V10.0.1.0.PDLMIFJ:user/release-keys
    
    Change-Id: Ibd2f1097f3fbc5b826cdc2d72e1720e0a3ab1b30

--- Android.mk
+++ Android.mk
@@ -78,7 +78,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT) $(DSP_MOUNT_POINT) $(PE
 
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
 
-IMS_SYMLINKS := $(addprefix $(TARGET_OUT_APPS)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_APPS_PRIVILEGED)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
 $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "IMS lib link: $@"
 	@mkdir -p $(dir $@)
