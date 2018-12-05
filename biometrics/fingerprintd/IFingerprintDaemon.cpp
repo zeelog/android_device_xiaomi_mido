96192737c2cfc9722d45ca99afb2904056fd14e2
Nich <nctrenco@gmail.com>
2018-12-05 12:02:11 +0800

    mido: biometrics: Fix compiler warning
    Change-Id: Id2454c152ed608b4abebe9f4329029a6b79e53e6

--- biometrics/fingerprintd/IFingerprintDaemon.cpp
+++ biometrics/fingerprintd/IFingerprintDaemon.cpp
@@ -188,7 +188,7 @@ class BpFingerprintDaemon : public BpInterface<IFingerprintDaemon> {
                 BpInterface<IFingerprintDaemon>(impl) {
         }
 
-        virtual void init(fingerprint_notify_t notify) {
+        virtual void init(fingerprint_notify_t __unused notify) {
             ALOGE("init()");
             return;
         }
