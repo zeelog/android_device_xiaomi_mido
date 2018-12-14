1e3105ca5695d4248f33271e364260712ba71443
ZeeLog <Danjel.Zammit@gmail.com>
2018-12-14 19:49:26 +0100

    mido: audio: Update product packages for P
    Change-Id: I97f8886d7e1d5f34c50962af02c6ced7e4473011

--- device.mk
+++ device.mk
@@ -79,15 +79,10 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio@2.0-service \
-    android.hardware.audio@4.0 \
     android.hardware.audio@4.0-impl \
-    android.hardware.audio.common@4.0 \
-    android.hardware.audio.common@4.0-util \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@4.0 \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.primary.msm8953 \
