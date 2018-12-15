2bf7bc85399cca2b1ff982788e6c9095a963c1c7
ZeeLog <Danjel.Zammit@gmail.com>
2018-12-16 00:11:12 +0100

    mido: SELinux: switch to Enforcing mode
    Change-Id: I7f7d5b2bd4953e7437040f2b7f0349cd1eb45a4f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,7 +41,6 @@ TARGET_USES_64_BIT_BINDER := true
 TARGET_KERNEL_CONFIG := mido_defconfig
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 firmware_class.path=/vendor/firmware_mnt/image
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
