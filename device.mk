7449c4be565020da90e4c2e324d81d48c696ced2
Paul McLean <pmclean@google.com>
2018-12-17 08:14:18 -0700

    mido: Adding Audio HAL V5: Direction API
    Bug: 119137468
    Test: With testbed app
    Change-Id: I4251b3853d00308dbd988625f8e7466a66b823fa

--- device.mk
+++ device.mk
@@ -79,9 +79,9 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.primary.msm8953 \
