544f29a65adc695be8aceec34976be1e9d9fbbaf
ZeeLog <Danjel.Zammit@gmail.com>
2018-12-22 11:59:18 +0100

    mido: re-add powerhint.xml
    Change-Id: I170f9d12625735c4dfa110dbd93cc711a990c9e1

--- device.mk
+++ device.mk
@@ -285,6 +285,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
