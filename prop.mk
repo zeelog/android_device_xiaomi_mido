4678ebb1ffe6ae90e40f2ea90afc245c7da8ee34
Bruno Martins <bgcngm@gmail.com>
2018-12-22 19:07:24 +0000

    mido: Enable B-services aging propagation
    Change-Id: I4ac0525933bb421f7d025c5e4c6a1de0f083c46e

--- prop.mk
+++ prop.mk
@@ -129,7 +129,8 @@ vendor.video.disable.ubwc=1
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.vendor.extension_library=libqti-perfd-client.so
+ro.vendor.extension_library=libqti-perfd-client.so \
+ro.vendor.qti.sys.fw.bservice_enable=true
 
 # Netmgrd
 PRODUCT_PROPERTY_OVERRIDES += \
