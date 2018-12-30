8ff58152ba26dd1624bafe45521ee00d37b929f9
ZeeLog <Danjel.Zammit@gmail.com>
2018-12-30 18:56:57 +0100

    mido: Switch to wcnss_service from Tissot
    * From xiaomi/tissot/tissot_sprout:9/PKQ1.180917.001/V10.0.2.0.PDHMIFK:user/release-keys
    * Add missing firmware
    * Append vendor prefix to wpa_wlan0
    * nuke wcnss_service sepolicy rules
    
    Change-Id: I3dad1d7552f6ae5d098c2fb1e6529194d6d7d0ed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -222,7 +222,6 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
-PRODUCT_VENDOR_MOVE_ENABLED := true
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/mido/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -382,13 +382,13 @@ PRODUCT_PACKAGES += \
     libQWiFiSoftApCfg \
     libwpa_client \
     hostapd \
-    wcnss_service \
     wpa_supplicant \
     wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Wi-Fi Display
