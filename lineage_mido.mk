4fb84b2815e2c6ebcfc5594cd6aaeb0e04db846d
ZeeLog <Danjel.Zammit@gmail.com>
2019-01-09 21:11:14 +0100

    mido: update build fingerprint from Tissot
    TEST:
    Change-Id: I6c3c69ce435d7aad1d1614c9ea222c935a2bc6c1
    SafetyNet: PASS

--- lineage_mido.mk
+++ lineage_mido.mk
@@ -36,7 +36,7 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="xiaomi/tissot/tissot_sprout:9/PKQ1.180917.001/V10.0.2.0.PDHMIFK:user/release-keys"
+    PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:9/PKQ1.180917.001/V10.0.2.0.PDHMIFK:user/release-keys"
+BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
