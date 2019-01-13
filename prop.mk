0af25d36bcdfc3e8ef33b1bd6687e114f908df9a
Zile995 <stefan.zivkovic995@gmail.com>
2019-01-13 13:29:54 +0000

    mido: Add dirac property
    Signed-off-by: Zile995 <stefan.zivkovic995@gmail.com>

--- prop.mk
+++ prop.mk
@@ -8,6 +8,7 @@ audio.deep_buffer.media=true \
 audio.offload.disable=true \
 audio.offload.min.duration.secs=30 \
 audio.offload.video=true \
+persist.audio.dirac.speaker=true \
 persist.vendor.audio.fluence.speaker=true \
 persist.vendor.audio.fluence.voicecall=true \
 persist.vendor.audio.fluence.voicerec=true \
