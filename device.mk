69a9b09c4dddf966e8467791fbb677aab3e0ec3b
Bruno Martins <bgcngm@gmail.com>
2019-01-21 09:40:51 +0000

    mido: Migrate to livedisplay 2.0
    Change-Id: I513c284c534ce3abc27bf374513280171454f4d1

--- device.mk
+++ device.mk
@@ -246,9 +246,9 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service \
     lights.msm8953
 
-# LiveDisplay native
+# LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
