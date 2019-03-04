2cc20ae045717ec24aa6a4bf766319ab6735fd3a
ZeeLog <Danjel.Zammit@gmail.com>
2019-03-04 20:53:24 +0100

    mido: media_codecs: fix derp in copy rule
    Change-Id: Ie9deef82f5a29ccfc7d60999706fdce82f52afea

--- device.mk
+++ device.mk
@@ -252,7 +252,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_codecs.xml::$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
