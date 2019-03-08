45ecd59b5211cf5ae7b9c38e7e9eefa8022f2415
merothh <merothh@gmail.com>
2019-03-08 14:25:34 +0530

    mido: make extraction scripts work with common vendor

--- extract-files.sh
+++ extract-files.sh
@@ -19,6 +19,7 @@
 set -e
 
 DEVICE=mido
+DEVICE_COMMON=msm8953-common
 VENDOR=xiaomi
 
 # Load extract_utils and do some sanity checks
@@ -56,19 +57,25 @@ if [ -z "$SRC" ]; then
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true "$CLEAN_VENDOR"
 
-extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 extract "$MY_DIR"/proprietary-files-qc.txt "$SRC" "$SECTION"
 
-DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+if [ -s "$MY_DIR"/proprietary-files.txt ]; then
+    # Reinitialize the helper for device
+    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
-sed -i \
-    's/\/system\/etc\//\/vendor\/etc\//g' \
-    "$DEVICE_BLOB_ROOT"/vendor/lib/libmmcamera2_sensor_modules.so
+    extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 
-sed -i \
-     "s|/data/misc/camera/cam_socket|/data/vendor/qcam/cam_socket|g" \
-     "$DEVICE_BLOB_ROOT"vendor/bin/mm-qcamera-daemon
+    DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+    sed -i \
+        's/\/system\/etc\//\/vendor\/etc\//g' \
+        "$DEVICE_BLOB_ROOT"/vendor/lib/libmmcamera2_sensor_modules.so
+
+    sed -i \
+         "s|/data/misc/camera/cam_socket|/data/vendor/qcam/cam_socket|g" \
+         "$DEVICE_BLOB_ROOT"/vendor/bin/mm-qcamera-daemon
+fi
 
 "$MY_DIR"/setup-makefiles.sh
