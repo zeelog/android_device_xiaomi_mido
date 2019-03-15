bc18f5a5b91942da14b5c777ebf2b16cec757a91
dianlujitao <dianlujitao@lineageos.org>
2019-03-15 14:05:20 +0800

    mido: Remove fake android.hidl.manager@1.0.so
     * No longer needed with Pie blobs
    
    Change-Id: Ie5a71a9832f6c79ba67a14272f37a3b1ff898c55

--- device.mk
+++ device.mk
@@ -204,8 +204,7 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0
 
 # IMS
 PRODUCT_PACKAGES += \
--- vndk/Android.mk
+++ vndk/Android.mk
@@ -35,8 +35,7 @@ VNDK_SP_LIBRARIES := \
     libz
 
 EXTRA_VENDOR_LIBRARIES := \
-    android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0
 
 #-------------------------------------------------------------------------------
 # VNDK Modules
