3e71e5b79532feeeb0c55b3d5fe5467bf7b1a431
ZeeLog <Danjel.Zammit@gmail.com>
2019-03-17 16:53:24 +0100

    mido: GPS: build libsensorndkbridge from source
    Change-Id: I9786aba835a3f22df02466c390638e257207d593

--- device.mk
+++ device.mk
@@ -186,7 +186,8 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl-qti \
     libcurl \
     libgnss \
-    libgnsspps
+    libgnsspps \
+    libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
