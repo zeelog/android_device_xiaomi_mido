f8b46a36591b89dbad8856bc924dc968ac79a4dc
ZeeLog <Danjel.Zammit@gmail.com>
2019-03-18 13:16:26 +0100

    mido: location: remove useless slim_daemon
    Change-Id: I203c665b305b6c979d551287598dd9efbb38669a

--- config.fs
+++ config.fs
@@ -58,12 +58,6 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
 
-[vendor/bin/slim_daemon]
-mode: 0755
-user:  AID_GPS
-group: AID_GPS
-caps: NET_BIND_SERVICE
-
 [vendor/bin/loc_launcher]
 mode: 0755
 user:  AID_GPS
