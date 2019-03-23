ce432281cf1f8ea7a149e1a4f87cdad6a36386eb
ZeeLog <Danjel.Zammit@gmail.com>
2019-03-23 15:09:41 +0100

    mido: wifi: build from vendor tree
    * cleanup wifi symlinks
    * update proprietary_files
    
    Change-Id: I46941e6749c17aca9bdf0b32ee8f8422e0acce29

--- device.mk
+++ device.mk
@@ -395,7 +395,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Wi-Fi Display
