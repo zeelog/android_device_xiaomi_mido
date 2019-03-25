63120de89b9e7ecf45aeffbe2445bde740479671
Nich <nctrenco@gmail.com>
2019-03-25 15:33:01 -0400

    mido: camera: Saner workaround for dark preview issue
    * Update comments to clarify that the issue was indeed caused by a
      backend bug.
    
    Change-Id: Id2514c6bf2e02e7d1e5062081fa8a641f7db01fd

--- camera/QCamera2/HAL/QCameraParameters.cpp
+++ camera/QCamera2/HAL/QCameraParameters.cpp
@@ -6575,6 +6575,15 @@ int32_t QCameraParameters::setPreviewFpsRange(int min_fps,
     property_get("persist.debug.set.fixedfps", value, "0");
     fixedFpsValue = atoi(value);
 
+    // Workaround backend AEC bug that doesn't set high enough ISO values when the min FPS value
+    // is higher than expected, which resulted in a very dark preview in low light conditions
+    // while recording. The lowest FPS value AEC expects in such conditions is 19.99, so 15fps
+    // as the min FPS value should be sufficient.
+    if (!isHfrMode() && min_fps > 15000) {
+        LOGH("Original min_fps %d, changing min_fps to 15000", min_fps);
+        min_fps = 15000;
+    }
+
     LOGD("E minFps = %d, maxFps = %d , vid minFps = %d, vid maxFps = %d",
                  min_fps, max_fps, vid_min_fps, vid_max_fps);
 
