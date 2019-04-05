4c227461f2ee2fd7ae7d00d00a16a8673e79bccc
Artem Borisov <dedsa2002@gmail.com>
2019-04-05 19:52:44 +0530

    mido: Re-enable some UX optimizations from stock
    * These were removed as I blamed them for UI performance degradation.
    * Further testing shows that they actually reduce jank in some cases.
    
    Change-Id: I6f3ae240366cc49e1a25d2f76ddd847ced0fb8f4

--- prop.mk
+++ prop.mk
@@ -73,6 +73,7 @@ debug.gralloc.enable_fb_ubwc=1 \
 debug.mdpcomp.logs=0 \
 debug.sf.enable_hwc_vds=1 \
 debug.sf.hw=0 \
+debug.sf.latch_unsignaled=1 \
 debug.sf.recomputecrop=0 \
 dev.pm.dyn_samplingrate=1 \
 persist.demo.hdmirotationlock=false \
@@ -186,6 +187,10 @@ persist.timed.enable=true
 # Tcp
 PRODUCT_PROPERTY_OVERRIDES += \
 net.tcp.2g_init_rwnd=10
+
+# UI
+PRODUCT_PROPERTY_OVERRIDES += \
+sys.use_fifo_ui=1
 
 # Usb
 PRODUCT_PROPERTY_OVERRIDES += \
