fd584917aacd429a30bd71f36eb51462ae11ef59
ZeeLog <Danjel.Zammit@gmail.com>
2019-04-21 01:04:58 +0200

    mido: Update Graphics & Media stack from OnePlus3T
    * From: OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1903190014:user/release-keys
    
    Change-Id: I7192d0b0a9598d2259b97f1d3974da4d37a9480c

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -36,10 +36,7 @@ VNDK_SP_LIBRARIES := \
 
 EXTRA_VENDOR_LIBRARIES := \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0 \
-    vendor.display.color@1.0 \
-    vendor.display.config@1.0
-
+    android.hidl.manager@1.0
 
 #-------------------------------------------------------------------------------
 # VNDK Modules
