fedcc6e60eacba36a2e6411e9191c250c0530a6a
ZeeLog <Danjel.Zammit@gmail.com>
2019-04-21 12:52:43 +0200

    mido: Update security patch level
    Change-Id: Ie08b2564a6a416bd43dd6d3c9fbd0c5109deac7c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -195,7 +195,7 @@ TARGET_RIL_VARIANT := caf
 TARGET_USES_OLD_MNC_FORMAT := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-01-05
+VENDOR_SECURITY_PATCH := 2019-04-01
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
