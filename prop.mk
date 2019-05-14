d31c41bff29530d21a05c446fa4fd1139ff083ea
davidtrpcevski <trpcevski.david@gmail.com>
2019-05-14 12:19:48 +0200

    mido: Remove duplicate line for enabling display default color mode
    * Already defined
    
    Signed-off-by: davidtrpcevski <trpcevski.david@gmail.com>

--- prop.mk
+++ prop.mk
@@ -85,7 +85,6 @@ ro.vendor.display.cabl=2 \
 sdm.debug.disable_skip_validate=1 \
 vendor.display.enable_default_color_mode=1 \
 vendor.display.disable_skip_validate=1 \
-vendor.display.enable_default_color_mode=1 \
 vendor.gralloc.enable_fb_ubwc=1
 
 # DRM
