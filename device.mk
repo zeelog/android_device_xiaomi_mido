b24befe86ab6c6a89a5e8b5f50e9312d57a53e95
ZeeLog <Danjel.Zammit@gmail.com>
2019-05-30 17:47:25 +0200

    Revert "mido: build separate system and vendor hidl libs"
    This reverts commit 3317e08b46507316d49e4186ea03272266aefa0d.

--- device.mk
+++ device.mk
@@ -205,9 +205,7 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.manager@1.0
 
 # IMS
 PRODUCT_PACKAGES += \
