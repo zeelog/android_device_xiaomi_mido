17d59152c94059a0de5383a59715970cd6366a67
Kevin F. Haggerty <haggertk@lineageos.org>
2019-06-12 17:51:39 +0000

    mido: Set TARGET_KERNEL_CROSS_COMPILE_PREFIX
    * We need to make sure to use the aarch64-linux-android-
      toolchain due to kernel GCC_TOOLCHAIN_DIR logic updates.
    
    Change-Id: Ib75c5f06b27ab8979d28a3446e36d73e29846769

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,6 +44,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
 
 # ANT
