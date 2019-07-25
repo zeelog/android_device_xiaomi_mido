11dd55e3edf40d1c6cdc74f366ce9d575a75f09d
Abhishek Pant <apant@codeaurora.org>
2019-07-25 13:09:59 +0200

    mido: camera: QCamera2: Fix hang during snapshot after flush
    Issue:
    If snapshot is triggered after flush in HAL 3 sometimes
    it is leading to a hang. If there is a blob capture
    request by the app for which HAL has queued the setting
    in jpeg setting queue but before de-queuing these setting
    if flush is called HAL wont flush the jpeg setting queue.
    After flush if another blob capture request is triggered
    it will fail because HAL would try to use the setting for
    the previous jpeg job which were not flushed.
    
    Fix:
    Flush the jpeg setting queue when data proc is stopped.
    Initialize the jpeg setting queue when data proc is started.
    
    Change-Id: I9b533e75fb9d87984fb9c3c7f4ce34231403b792
    Signed-off-by: David Trpchevski <trpcevski.david@gmail.com>

--- camera/QCamera2/HAL3/QCamera3PostProc.cpp
+++ camera/QCamera2/HAL3/QCamera3PostProc.cpp
@@ -1990,6 +1990,7 @@ void *QCamera3PostProcessor::dataProcessRoutine(void *data)
             pme->m_inputPPQ.init();
             pme->m_inputFWKPPQ.init();
             pme->m_inputMetaQ.init();
+            pme->m_jpegSettingsQ.init();
             cam_sem_post(&cmdThread->sync_sem);
 
             break;
@@ -2031,6 +2032,7 @@ void *QCamera3PostProcessor::dataProcessRoutine(void *data)
                 pme->m_inputFWKPPQ.flush();
 
                 pme->m_inputMetaQ.flush();
+                pme->m_jpegSettingsQ.flush();
 
                 // signal cmd is completed
                 cam_sem_post(&cmdThread->sync_sem);
