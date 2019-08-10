6a087cd46993db5a861c92d36fad09dacf323d79
Murat Kozan <muratkozan350@gmail.com>
2019-08-10 18:08:37 +0300

    mido: Enable lockscreen charging values

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,6 +96,9 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # CNE and DPM
 BOARD_USES_QCNE := true
 
