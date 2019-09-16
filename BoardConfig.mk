eeb8c895fbbae2dab0ae2f81db4fbe18bc5ec7f1
dianlujitao <dianlujitao@lineageos.org>
2019-09-16 19:45:27 +0800

    mido: Remove unused vendor_framework_compatibility_matrix.xml
    Change-Id: Ic05de5e2094fb918523c74bbdf0f95b2176454e3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,7 +146,6 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 BOARD_HAVE_QCOM_FM := true
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/vendor_framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
