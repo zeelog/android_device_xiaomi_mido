24a81b6da69503404e55e2d17783ad59c5d23b50
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-16 19:02:36 +0000

    mido: Correct define libinit variable
    Fixes:
    
    15:55:35 error: system/core/init/Android.bp:95:1: "libinit" depends on undefined module "//device/xiaomi/mido/init/libinit_mido"
    
    Change-Id: Ia1a58bea3df625fd76c060370dc7f9fa2a597765

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,7 +151,7 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_mido
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_mido
 TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953
 
 # Lights
