7467b15e42e8db5e0466bc82ab14689bebf5b6a6
ZeeLog <Danjel.Zammit@gmail.com>
2019-09-16 09:24:22 +0200

    mido: camera: Suppress build warnings
    Change-Id: Ia41b2c0a95beec67ed23bf1fe8c19ce2554773d5

--- camera/QCamera2/HAL/QCameraStateMachine.cpp
+++ camera/QCamera2/HAL/QCameraStateMachine.cpp
@@ -638,7 +638,9 @@ int32_t QCameraStateMachine::procEvtPreviewStoppedState(qcamera_sm_evt_enum_t ev
             LOGW("Free video handle %d %d", evt, m_state);
             QCameraVideoMemory::closeNativeHandle((const void *)payload);
         }
+	[[fallthrough]];
     case QCAMERA_SM_EVT_PRE_START_RECORDING:
+	break;
     case QCAMERA_SM_EVT_RESTART_STOP_PREVIEW:
     case QCAMERA_SM_EVT_RESTART_START_PREVIEW:
     case QCAMERA_SM_EVT_START_RECORDING:
@@ -1058,7 +1060,9 @@ int32_t QCameraStateMachine::procEvtPreviewReadyState(qcamera_sm_evt_enum_t evt,
             LOGW("Free video handle %d %d", evt, m_state);
             QCameraVideoMemory::closeNativeHandle((const void *)payload);
         }
+	[[fallthrough]];
     case QCAMERA_SM_EVT_PRE_START_RECORDING:
+	break;
     case QCAMERA_SM_EVT_RESTART_STOP_PREVIEW:
     case QCAMERA_SM_EVT_RESTART_START_PREVIEW:
     case QCAMERA_SM_EVT_START_RECORDING:
@@ -1594,7 +1598,9 @@ int32_t QCameraStateMachine::procEvtPreviewingState(qcamera_sm_evt_enum_t evt,
             LOGW("Free video handle %d %d", evt, m_state);
             QCameraVideoMemory::closeNativeHandle((const void *)payload);
         }
+	[[fallthrough]];
     case QCAMERA_SM_EVT_RELEASE:
+	break;
         {
             LOGE("Error!! cannot handle evt(%d) in state(%d)", evt, m_state);
             rc = INVALID_OPERATION;
