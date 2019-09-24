e56b71e996d3671aa0175bb591374d0d0aaf8bc5
Nich <nctrenco@gmail.com>
2019-09-24 04:09:03 +0000

    mido: biometrics: Start threadpool on vndbiner
    Vendor processes should not use /dev/binder.
    
    Change-Id: I4a54bae29dd9ef05109a21f35ab262b33c7542a7

--- biometrics/service.cpp
+++ biometrics/service.cpp
@@ -61,7 +61,7 @@ int main() {
         }
 
         // the conventional HAL might start binder services
-        android::ProcessState::initWithDriver("/dev/binder");
+        android::ProcessState::initWithDriver("/dev/vndbinder");
         android::ProcessState::self()->startThreadPool();
     }
 
