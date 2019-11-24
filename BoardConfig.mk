85c21dcac3f4a47f8bcc6868067149de2dbcd914
ZeeLog <Danjel.Zammit@gmail.com>
2019-11-24 22:10:21 +0100

    mido: Consolidate proprietary-files
    Change-Id: Idd6d7421156d32b9108948187729a8074e756364

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -217,4 +217,3 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/mido/BoardConfigVendor.mk
--include vendor/xiaomi/msm8953-common/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -15,7 +15,6 @@
 #
 
 $(call inherit-product, vendor/xiaomi/mido/mido-vendor.mk)
-$(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Overlays
