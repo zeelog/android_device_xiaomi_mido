69fc3aaafa02dbea304db62f2c00c1fe204f85c0
ZeeLog <Danjel.Zammit@gmail.com>
2019-11-30 12:37:54 +0100

    mido: Refactor update-sha1sums.py script
    Change-Id: I1a008a16275df0137cb2ff8d93c5a144c7cb3b5d

--- update-sha1sums.py
+++ update-sha1sums.py
@@ -19,10 +19,10 @@
 from hashlib import sha1
 import sys
 
-device='msm8953-common'
+device='mido'
 vendor='xiaomi'
 
-lines = [ line for line in open('proprietary-files-qc.txt', 'r') ]
+lines = [ line for line in open('proprietary-files.txt', 'r') ]
 vendorPath = '../../../vendor/' + vendor + '/' + device + '/proprietary'
 needSHA1 = False
 
@@ -72,7 +72,7 @@ if len(sys.argv) == 2 and sys.argv[1] == '-c':
 else:
   update()
 
-with open('proprietary-files-qc.txt', 'w') as file:
+with open('proprietary-files.txt', 'w') as file:
   for line in lines:
     file.write(line)
 
