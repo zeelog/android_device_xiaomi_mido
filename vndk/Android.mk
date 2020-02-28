03ef4597a6c78d74cc4fa4738821eca23ed3c3d4
ZeeLog <Danjel.Zammit@gmail.com>
2020-02-28 09:58:29 +0100

    mido: Build display config (1-9) extra vendor libs
    Change-Id: Id3092139212732c572670460bb8d5e5461163533

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -41,6 +41,16 @@ VNDK_SP_LIBRARIES := \
 
 EXTRA_VENDOR_LIBRARIES := \
     android.hidl.base@1.0 \
+    vendor.display.config@1.0 \
+    vendor.display.config@1.1 \
+    vendor.display.config@1.2 \
+    vendor.display.config@1.3 \
+    vendor.display.config@1.4 \
+    vendor.display.config@1.5 \
+    vendor.display.config@1.6 \
+    vendor.display.config@1.7 \
+    vendor.display.config@1.8 \
+    vendor.display.config@1.9 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.cryptfshw@1.0
 
