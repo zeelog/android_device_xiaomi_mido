fae81cf23e7b47304867a47b468364027b9b0456
ZeeLog <Danjel.Zammit@gmail.com>
2020-03-08 23:50:22 +0100

    mido: biometrics: use prebuilt modified blobs
    * nuke biometrics
    * pin modified fp blobs
    * build android.hardware.biometrics.fingerprint@2.1 from source
    
    Change-Id: I75fdb0a3ff3682238b13cb121318b1d0eb56610c

--- device.mk
+++ device.mk
@@ -165,7 +165,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_mido
+    android.hardware.biometrics.fingerprint@2.1
 
 # FM
 PRODUCT_PACKAGES += \
