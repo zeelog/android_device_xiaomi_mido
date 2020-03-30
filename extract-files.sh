719b58d8e8922ab33e03a6e2be23a26337414605
Albert I <kras@raphielgang.org>
2020-03-30 21:28:32 +0800

    mido: Clean up extract-files
    Now that we have proprietary-files-qc.txt merged...
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- extract-files.sh
+++ extract-files.sh
@@ -19,7 +19,6 @@
 set -e
 
 DEVICE=mido
-DEVICE_COMMON=msm8953-common
 VENDOR=xiaomi
 
 # Load extract_utils and do some sanity checks
@@ -57,29 +56,19 @@ if [ -z "$SRC" ]; then
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true "$CLEAN_VENDOR"
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
-extract "$MY_DIR"/proprietary-files-qc.txt "$SRC" "$SECTION"
+extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 
-if [ -s "$MY_DIR"/proprietary-files.txt ]; then
-    # Reinitialize the helper for device
-    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
+DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
 
-    extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+# Camera configs
+sed -i "s|/system/etc/camera|/vendor/etc/camera|g" "$DEVICE_BLOB_ROOT"/vendor/lib/libmmcamera2_sensor_modules.so
 
-    DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+# Camera socket
+sed -i "s|/data/misc/camera/cam_socket|/data/vendor/qcam/cam_socket|g" "$DEVICE_BLOB_ROOT"/vendor/bin/mm-qcamera-daemon
 
-    sed -i \
-        's/\/system\/etc\//\/vendor\/etc\//g' \
-        "$DEVICE_BLOB_ROOT"/vendor/lib/libmmcamera2_sensor_modules.so
-
-    sed -i \
-         "s|/data/misc/camera/cam_socket|/data/vendor/qcam/cam_socket|g" \
-         "$DEVICE_BLOB_ROOT"/vendor/bin/mm-qcamera-daemon
-
-    sed -i \
-         "s|persist.camera.debug.logfile|persist.vendor.camera.dbglog|g" \
-         "DEVICE_BLOB_ROOT"/vendor/lib/libmmcamera_dbg.so
-fi
+# Camera debug log file
+sed -i "s|persist.camera.debug.logfile|persist.vendor.camera.dbglog|g" "$DEVICE_BLOB_ROOT"/vendor/lib/libmmcamera_dbg.so
 
 "$MY_DIR"/setup-makefiles.sh
