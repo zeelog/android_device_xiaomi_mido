e2465fc58b81ec63a63ccfc0b14ae649bbaeb34b
Sultan Alsawaf <sultan@kerneltoast.com>
2020-05-02 23:27:59 +0200

    mido: Force triple frame buffers
    This yields significantly reduced jitter and doesn't have any measurable
    impact on latency.
    
    Change-Id: I1c82d32e2fefd73779773edfc75265e992c0dad2

--- prop.mk
+++ prop.mk
@@ -79,6 +79,7 @@ persist.hwc.enable_vds=1 \
 persist.hwc.mdpcomp.enable=true \
 ro.opengles.version=196610 \
 ro.qualcomm.cabl=0 \
+ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
 ro.vendor.display.cabl=2 \
 sdm.debug.disable_skip_validate=1 \
 vendor.display.enable_default_color_mode=1 \
