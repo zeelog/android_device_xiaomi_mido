9a2648387960a4631a074d992c79bdc6517bab7b
Anusha Shakarad <ashaka@codeaurora.org>
2020-05-15 19:11:24 +0200

    mido: disable SF backpressure
    Change-Id: I3bacf63cdeab50be41306e8d58eed1c0d9316258

--- prop.mk
+++ prop.mk
@@ -72,6 +72,7 @@ debug.sf.enable_hwc_vds=1 \
 debug.sf.hw=0 \
 debug.sf.latch_unsignaled=1 \
 debug.sf.recomputecrop=0 \
+debug.sf.disable_backpressure=1 \
 debug.sf.enable_gl_backpressure=1 \
 dev.pm.dyn_samplingrate=1 \
 persist.demo.hdmirotationlock=false \
