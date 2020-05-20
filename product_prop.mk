ab9e82c0dcd0a0bf51d90f822f242fb84c310f1c
Albert I <kras@raphielgang.org>
2020-05-20 16:58:01 +0800

    mido: Use product.prop to set persistent I/O scheduler property
    * So we can steal setting it earlier at boot
    * Also rename prop.mk to vendor_prop.mk to avoid confusion
    
    This reverts commit bd8465f45d5bc6cc7aa4a53b14c40d424d9112f4.
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: Ie47b34ec8b4416e3e425ced6d243806c047517df

---
+++ product_prop.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2020 AIMROM
+# Copyright (C) 2020 KudProject Development
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Default to BFQ I/O scheduler
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.io.scheduler=bfq
