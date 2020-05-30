016baee0e3083459d270f41a8b13417b0aa30c01
LuK1337 <priv.luk@gmail.com>
2020-05-30 18:00:45 +0200

    mido: Drop qti-telephony-common aosp_usr_pref_sel
    * Seems like mobile data doesn't get toggled
      properly after doing this...
    
    Change-Id: I1ba996aec5a80bd7de8c1bddbd4a2a33b8146e57

--- prop.mk
+++ prop.mk
@@ -168,7 +168,6 @@ persist.vendor.radio.custom_ecc=1 \
 persist.vendor.radio.hw_mbn_update=0 \
 persist.vendor.radio.rat_on=combine \
 persist.vendor.radio.sib16_support=1 \
-persist.vendor.radio.aosp_usr_pref_sel=true \
 ril.subscription.types=NV,RUIM \
 rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
 ro.telephony.call_ring.multiple=false \
