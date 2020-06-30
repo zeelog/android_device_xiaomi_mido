8aa8649238e176f769c6247ed62c143c77205006
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-06-30 21:40:12 +0200

    Revert "mido: props: Enable zygote preforking"
    This reverts commit cbc211b5846ca0dad7541adb3a81efedae1ba25b.
    
    Reason for revert: Don't change default system behavior
    
    Change-Id: I35d50610925a9e18a8b96aa67422feb56c899e88

--- prop.mk
+++ prop.mk
@@ -200,7 +200,3 @@ wifi.interface=wlan0
 # Wifi-Display
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.sys.wfd.virtual=0
-
-# Zygote preforking
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.device_config.runtime_native.usap_pool_enabled=true
