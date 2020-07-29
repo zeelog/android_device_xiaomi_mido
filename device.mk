7eab411226182b2a0c93ad57af286d6e71465b29
Ethan Chen <intervigil@gmail.com>
2020-07-29 09:52:05 +0200

    mido: Switch to lazy WiFi HAL
    Change-Id: Ic5aca40b64ec85759703675a826f9fae0dbc58e9

--- device.mk
+++ device.mk
@@ -384,7 +384,7 @@ PRODUCT_COPY_FILES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service-lazy \
     libcld80211 \
     libwpa_client \
     hostapd \
