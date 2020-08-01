2b3746d3a8f1ae0cb6c90bf9acfc6d41c085817e
ShihabZzz <Shihab.riadn@gmail.com>
2020-08-01 22:46:57 +0600

    mido: livedisplay: Update C/C++ code format
    Change-Id: I55f9bff539342a8a974d16bd26a877be34484344

--- livedisplay/PictureAdjustment.h
+++ livedisplay/PictureAdjustment.h
@@ -29,42 +29,48 @@ using ::android::hardware::Return;
 using ::android::hardware::Void;
 
 class PictureAdjustment : public IPictureAdjustment {
-  public:
-    PictureAdjustment(void* libHandle, uint64_t cookie);
+public:
+  PictureAdjustment(void *libHandle, uint64_t cookie);
 
-    bool isSupported();
+  bool isSupported();
 
-    // Methods from ::vendor::lineage::livedisplay::V2_0::IPictureAdjustment follow.
-    Return<void> getHueRange(getHueRange_cb _hidl_cb) override;
-    Return<void> getSaturationRange(getSaturationRange_cb _hidl_cb) override;
-    Return<void> getIntensityRange(getIntensityRange_cb _hidl_cb) override;
-    Return<void> getContrastRange(getContrastRange_cb _hidl_cb) override;
-    Return<void> getSaturationThresholdRange(getSaturationThresholdRange_cb _hidl_cb) override;
-    Return<void> getPictureAdjustment(getPictureAdjustment_cb _hidl_cb) override;
-    Return<void> getDefaultPictureAdjustment(getDefaultPictureAdjustment_cb _hidl_cb) override;
-    Return<bool> setPictureAdjustment(
-            const ::vendor::lineage::livedisplay::V2_0::HSIC& hsic) override;
+  // Methods from ::vendor::lineage::livedisplay::V2_0::IPictureAdjustment
+  // follow.
+  Return<void> getHueRange(getHueRange_cb _hidl_cb) override;
+  Return<void> getSaturationRange(getSaturationRange_cb _hidl_cb) override;
+  Return<void> getIntensityRange(getIntensityRange_cb _hidl_cb) override;
+  Return<void> getContrastRange(getContrastRange_cb _hidl_cb) override;
+  Return<void>
+  getSaturationThresholdRange(getSaturationThresholdRange_cb _hidl_cb) override;
+  Return<void> getPictureAdjustment(getPictureAdjustment_cb _hidl_cb) override;
+  Return<void>
+  getDefaultPictureAdjustment(getDefaultPictureAdjustment_cb _hidl_cb) override;
+  Return<bool> setPictureAdjustment(
+      const ::vendor::lineage::livedisplay::V2_0::HSIC &hsic) override;
 
-    static void updateDefaultPictureAdjustment();
+  static void updateDefaultPictureAdjustment();
 
-  private:
-    void* mLibHandle;
-    uint64_t mCookie;
+private:
+  void *mLibHandle;
+  uint64_t mCookie;
 
-    int32_t (*disp_api_get_feature_version)(uint64_t, uint32_t, void*, uint32_t*);
-    int32_t (*disp_api_get_global_pa_range)(uint64_t, uint32_t, void*);
-    int32_t (*disp_api_get_global_pa_config)(uint64_t, uint32_t, uint32_t*, void*);
-    int32_t (*disp_api_set_global_pa_config)(uint64_t, uint32_t, uint32_t, void*);
+  int32_t (*disp_api_get_feature_version)(uint64_t, uint32_t, void *,
+                                          uint32_t *);
+  int32_t (*disp_api_get_global_pa_range)(uint64_t, uint32_t, void *);
+  int32_t (*disp_api_get_global_pa_config)(uint64_t, uint32_t, uint32_t *,
+                                           void *);
+  int32_t (*disp_api_set_global_pa_config)(uint64_t, uint32_t, uint32_t,
+                                           void *);
 
-    HSIC getPictureAdjustmentInternal();
+  HSIC getPictureAdjustmentInternal();
 
-    HSIC mDefaultPictureAdjustment;
+  HSIC mDefaultPictureAdjustment;
 };
 
-}  // namespace sdm
-}  // namespace V2_0
-}  // namespace livedisplay
-}  // namespace lineage
-}  // namespace vendor
+} // namespace sdm
+} // namespace V2_0
+} // namespace livedisplay
+} // namespace lineage
+} // namespace vendor
 
-#endif  // VENDOR_LINEAGE_LIVEDISPLAY_V2_0_PICTUREADJUSTMENT_H
+#endif // VENDOR_LINEAGE_LIVEDISPLAY_V2_0_PICTUREADJUSTMENT_H
--- livedisplay/service.cpp
+++ livedisplay/service.cpp
@@ -34,87 +34,91 @@ using ::vendor::lineage::livedisplay::V2_0::IPictureAdjustment;
 using ::vendor::lineage::livedisplay::V2_0::sdm::PictureAdjustment;
 
 int main() {
-    // Vendor backend
-    void* libHandle = nullptr;
-    const char* libName = "libsdm-disp-vndapis.so";
-    int32_t (*disp_api_init)(uint64_t*, uint32_t) = nullptr;
-    int32_t (*disp_api_deinit)(uint64_t, uint32_t) = nullptr;
-    uint64_t cookie = 0;
+  // Vendor backend
+  void *libHandle = nullptr;
+  const char *libName = "libsdm-disp-vndapis.so";
+  int32_t (*disp_api_init)(uint64_t *, uint32_t) = nullptr;
+  int32_t (*disp_api_deinit)(uint64_t, uint32_t) = nullptr;
+  uint64_t cookie = 0;
 
-    // HIDL frontend
-    sp<PictureAdjustment> pa;
+  // HIDL frontend
+  sp<PictureAdjustment> pa;
 
-    status_t status = OK;
+  status_t status = OK;
 
-    android::ProcessState::initWithDriver("/dev/vndbinder");
+  android::ProcessState::initWithDriver("/dev/vndbinder");
 
-    LOG(INFO) << "LiveDisplay HAL service is starting.";
+  LOG(INFO) << "LiveDisplay HAL service is starting.";
 
-    libHandle = dlopen(libName, RTLD_NOW);
+  libHandle = dlopen(libName, RTLD_NOW);
 
-    if (libHandle == nullptr) {
-        LOG(ERROR) << "Failed to load SDM display lib, exiting.";
-        goto shutdown;
-    }
+  if (libHandle == nullptr) {
+    LOG(ERROR) << "Failed to load SDM display lib, exiting.";
+    goto shutdown;
+  }
 
-    disp_api_init =
-            reinterpret_cast<int32_t (*)(uint64_t*, uint32_t)>(dlsym(libHandle, "disp_api_init"));
-    if (disp_api_init == nullptr) {
-        LOG(ERROR) << "Can not get disp_api_init from " << libName << " (" << dlerror() << ")";
-        goto shutdown;
-    }
+  disp_api_init = reinterpret_cast<int32_t (*)(uint64_t *, uint32_t)>(
+      dlsym(libHandle, "disp_api_init"));
+  if (disp_api_init == nullptr) {
+    LOG(ERROR) << "Can not get disp_api_init from " << libName << " ("
+               << dlerror() << ")";
+    goto shutdown;
+  }
 
-    disp_api_deinit =
-            reinterpret_cast<int32_t (*)(uint64_t, uint32_t)>(dlsym(libHandle, "disp_api_deinit"));
-    if (disp_api_deinit == nullptr) {
-        LOG(ERROR) << "Can not get disp_api_deinit from " << libName << " (" << dlerror() << ")";
-        goto shutdown;
-    }
+  disp_api_deinit = reinterpret_cast<int32_t (*)(uint64_t, uint32_t)>(
+      dlsym(libHandle, "disp_api_deinit"));
+  if (disp_api_deinit == nullptr) {
+    LOG(ERROR) << "Can not get disp_api_deinit from " << libName << " ("
+               << dlerror() << ")";
+    goto shutdown;
+  }
 
-    status = disp_api_init(&cookie, 0);
+  status = disp_api_init(&cookie, 0);
+  if (status != OK) {
+    LOG(ERROR) << "Can not initialize " << libName << " (" << status << ")";
+    goto shutdown;
+  }
+
+  pa = new PictureAdjustment(libHandle, cookie);
+  if (pa == nullptr) {
+    LOG(ERROR) << "Can not create an instance of LiveDisplay HAL "
+                  "PictureAdjustment Iface, "
+                  "exiting.";
+    goto shutdown;
+  }
+
+  if (!pa->isSupported()) {
+    // Backend isn't ready yet, so restart and try again
+    goto shutdown;
+  }
+
+  configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+  if (pa->isSupported()) {
+    status = pa->registerAsService();
     if (status != OK) {
-        LOG(ERROR) << "Can not initialize " << libName << " (" << status << ")";
-        goto shutdown;
+      LOG(ERROR) << "Could not register service for LiveDisplay HAL "
+                    "PictureAdjustment Iface ("
+                 << status << ")";
+      goto shutdown;
     }
+  }
 
-    pa = new PictureAdjustment(libHandle, cookie);
-    if (pa == nullptr) {
-        LOG(ERROR) << "Can not create an instance of LiveDisplay HAL PictureAdjustment Iface, "
-                      "exiting.";
-        goto shutdown;
-    }
-
-    if (!pa->isSupported()) {
-        // Backend isn't ready yet, so restart and try again
-        goto shutdown;
-    }
-
-    configureRpcThreadpool(1, true /*callerWillJoin*/);
-
-    if (pa->isSupported()) {
-        status = pa->registerAsService();
-        if (status != OK) {
-            LOG(ERROR) << "Could not register service for LiveDisplay HAL PictureAdjustment Iface ("
-                       << status << ")";
-            goto shutdown;
-        }
-    }
-
-    LOG(INFO) << "LiveDisplay HAL service is ready.";
-    joinRpcThreadpool();
-    // Should not pass this line
+  LOG(INFO) << "LiveDisplay HAL service is ready.";
+  joinRpcThreadpool();
+  // Should not pass this line
 
 shutdown:
-    // Cleanup what we started
-    if (disp_api_deinit != nullptr) {
-        disp_api_deinit(cookie, 0);
-    }
+  // Cleanup what we started
+  if (disp_api_deinit != nullptr) {
+    disp_api_deinit(cookie, 0);
+  }
 
-    if (libHandle != nullptr) {
-        dlclose(libHandle);
-    }
+  if (libHandle != nullptr) {
+    dlclose(libHandle);
+  }
 
-    // In normal operation, we don't expect the thread pool to shutdown
-    LOG(ERROR) << "LiveDisplay HAL service is shutting down.";
-    return 1;
+  // In normal operation, we don't expect the thread pool to shutdown
+  LOG(ERROR) << "LiveDisplay HAL service is shutting down.";
+  return 1;
 }
