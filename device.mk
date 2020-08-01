01d6ec549e4fd5961ae078e6ae2151a837735235
ZeeLog <Danjel.Zammit@gmail.com>
2020-08-01 10:39:25 +0200

    mido: Restore previous wifi implementation
    This is a squashed update of the following commits:
    
    Revert "mido: Stop copying WCNSS_qcom_cfg.ini to userdata"
    This reverts commit 9b47c33e778488b65ce32a7434166ff582a6283a.
    
    Revert "mido: wifi: build from vendor tree"
    This reverts commit ce432281cf1f8ea7a149e1a4f87cdad6a36386eb.
    
    mido: sepolicy: address  wifi denials
    Change-Id: Ic77f3305178ab7c3f158804742f7ee23416363c7
    
    Revert "mido: wifi: Update and modify wcnss_service."
    This reverts commit 38dedadf54f2c193b0c8b4662b80c3c2c3dbdd87.
    
    Revert "mido: Provide module targets for libqmiservices"
    This reverts commit 4b88b9f3dcdc4d7c2a70c078d7f4abd12715c32a.

--- device.mk
+++ device.mk
@@ -388,7 +388,6 @@ PRODUCT_PACKAGES += \
     libcld80211 \
     libwpa_client \
     hostapd \
-    libqmiservices_shim \
     libwifi-hal-qcom \
     wpa_supplicant \
     wpa_supplicant.conf
@@ -396,7 +395,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Wi-Fi Display
 PRODUCT_PACKAGES += \
