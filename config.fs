a5c80f106e0a301b3053ce4ea9f133ca29f17c7f
ZeeLog <Danjel.Zammit@gmail.com>
2020-08-26 21:53:05 +0200

    Revert "mido: Remove firmware dirs from config.fs"
    This reverts commit 2a188d103edf73d092ba3ade78006d80abe0b324.
    
    Change-Id: I200cd9d6025ea1dd12c9f5581189e71f34e20876

--- config.fs
+++ config.fs
@@ -73,6 +73,18 @@ user:  AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE
 
+[firmware/]	
+mode: 0771	
+user: AID_SYSTEM	
+group: AID_SYSTEM	
+caps: 0	
+
+[firmware/image/*]	
+mode: 0771	
+user: AID_SYSTEM	
+group: AID_SYSTEM	
+caps: 0
+
 [vendor/firmware_mnt/image/*]
 mode: 0771
 user: AID_SYSTEM
@@ -83,4 +95,16 @@ caps: 0
 mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM
+caps: 0
+
+[persist/]	
+mode: 0771	
+user: AID_SYSTEM	
+group: AID_SYSTEM	
+caps: 0	
+
+[dsp/]	
+mode: 0771	
+user: AID_MEDIA	
+group: AID_MEDIA	
 caps: 0
