3c1282fa5228145ba73132a2dc251e3e11201ae4
LuK1337 <priv.luk@gmail.com>
2020-09-18 10:57:02 +0200

    mido: sepolicy: Update qcom sepolicy include
    Change-Id: I7167575e82eb8c5de2d822643261757edda73a07

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -197,7 +197,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 VENDOR_SECURITY_PATCH := 2020-08-01
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
