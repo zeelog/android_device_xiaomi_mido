caaf404cd4363cb729fb93aab9328d694d74254b
Arian <arian.kulmer@web.de>
2020-09-22 17:32:07 +0200

    mido: Add a shim for dpmframework
    * libdpmframework depends on strndup16to8 and strdup8to16 methods.
    
    * update sha1sum for modified blob
    Change-Id: I39d252b5830b752728a0c8607d32d8a279524ce1
    Co-authored-by: Demon000 <demonsingur@gmail.com>

--- device.mk
+++ device.mk
@@ -165,6 +165,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiDoze
 
+# DPM
+PRODUCT_PACKAGES += \
+    libshim_dpmframework
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
