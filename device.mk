3e71a466faa1484206d588784d49df548c211b7d
Davide Garberi <dade.garberi@gmail.com>
2020-09-22 12:31:43 +0200

    mido: Build libhidltransport and libhwbinder

--- device.mk
+++ device.mk
@@ -205,7 +205,11 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0
+    android.hidl.base@1.0 \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # HW crypto
 PRODUCT_PACKAGES += \
