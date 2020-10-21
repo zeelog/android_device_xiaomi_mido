94144bb1e60c6b174aaa946b8a91fd6113aff5fb
ZeeLog <Danjel.Zammit@gmail.com>
2020-10-21 06:20:34 +0200

    mido: Import vendor.qti.hardware.dsp.policy from LA.UM.9.6.2.r1-02100-89xx.0

--- device.mk
+++ device.mk
@@ -363,7 +363,8 @@ PRODUCT_BOOT_JARS += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp_policy/vendor.qti.hardware.dsp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/vendor.qti.hardware.dsp.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
