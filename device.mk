b8e0a8178db1550435acb01cd9537dc134c0d544
ZeeLog <Danjel.Zammit@gmail.com>
2020-10-24 10:29:44 +0200

    Drop shim for dpmframework
    * no longer needed
    
    This reverts commit caaf404cd4363cb729fb93aab9328d694d74254b.

--- device.mk
+++ device.mk
@@ -173,10 +173,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiDoze
 
-# DPM
-PRODUCT_PACKAGES += \
-    libshim_dpmframework
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
