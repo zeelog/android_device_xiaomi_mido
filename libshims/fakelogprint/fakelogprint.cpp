f6dfaafb3f42266f3684009f161d98278b20448d
tImIbreakdown <timifix@gmail.com>
2020-11-04 13:57:45 +0200

    mido: fakelogprint: update include
    * from zl1
    https://github.com/LineageOS/android_device_leeco_zl1/blob/lineage-17.1/libshims/fakelogprint/fakelogprint.cpp
    
    * fix: device/xiaomi/mido/libshims/fakelogprint/fakelogprint.cpp:1:10: fatal error: 'log/log_transport.h' file not found
    
    Change-Id: Idc17e3fd398a162ceecdbc4017f068e07613ed4b

--- libshims/fakelogprint/fakelogprint.cpp
+++ libshims/fakelogprint/fakelogprint.cpp
@@ -1,6 +1,4 @@
-#include <log/log_transport.h>
-#include <private/android_filesystem_config.h>
-#include <private/android_logger.h>
+#include <log/log.h>
 
 int __android_log_print(int prio, const char* tag, const char* fmt, ...) {
   // Goodix: please don't spam that much!
