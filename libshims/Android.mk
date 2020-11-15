LineageOS/android_device_leeco_zl1

Goodix FP HAL: Be quiet!
The FP HAL spams like hell when using the fingerprint reader.
Touching the reader with the correct finger causes 272(!) logcat lines. This slows the wake up significantly...
Since there is no switch built into the HAL to disable this output, just shim the whole log function to return nothing.

A good indicator of how much the spamming slows down the wake up are the physical buttons. They light up quite fast but the screen is activated only after all the logging is done.
With this shim in place the screen and buttons light up nearly simultaneously.

Change-Id: Ic51e8c2ddd7588fdb14a4ecf62b26dd8504c5e16

libshims/Android.mk
@@ -1,0 +1,31 @@
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# fake print lib for hexedited fingerprint libs
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog
+
+LOCAL_SRC_FILES := fakeprint/fakeprint.cpp
+
+#LOCAL_C_INCLUDES := \
+    system/core/liblog
+
+LOCAL_MODULE := fakelg
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)

zeelog/android_device_xiaomi_mido

libshims/Android.mk
@@ -1,0 +1,30 @@
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# fake print lib for hexedited fingerprint libs
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog
+
+LOCAL_SRC_FILES := fakelogprint/fakelogprint.cpp
+
+LOCAL_MODULE := fakelogprint
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)