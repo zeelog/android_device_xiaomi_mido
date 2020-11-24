9c9b3055fb1aeef4c4c3470929b8f5f6f435281d
Bruno Martins <bgcngm@gmail.com>
2020-11-24 21:11:23 +0100

    mido: camera: Switch to display_headers lib include
    LOCAL_COPY_HEADERS is deprecated and Qualcomm HALs have been
    updated to remove its usage. As a consequence, the display headers
    are no longer copied to TARGET_OUT_HEADERS, so use the equivalent
    header lib instead.

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -111,8 +111,6 @@ endif
 
 #LOCAL_STATIC_LIBRARIES := libqcamera2_util
 LOCAL_C_INCLUDES += \
-        $(TARGET_OUT_HEADERS)/qcom/display
-LOCAL_C_INCLUDES += \
         $(call project-path-for,qcom-display)/libqservice
 LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
@@ -126,6 +124,7 @@ endif
 ifeq ($(TARGET_TS_MAKEUP),true)
 LOCAL_SHARED_LIBRARIES += libts_face_beautify_hal libts_detected_face_hal
 endif
+LOCAL_HEADER_LIBRARIES += display_headers
 LOCAL_HEADER_LIBRARIES += camera_common_headers
 
 LOCAL_STATIC_LIBRARIES := android.hardware.camera.common@1.0-helper
