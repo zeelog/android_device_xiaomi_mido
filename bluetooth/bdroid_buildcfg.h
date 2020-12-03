6fae4f6907095d3213cd9e61a5971c245b922e29
Nolen Johnson <johnsonnolen@gmail.com>
2020-12-03 10:14:28 +0100

    mido: bluetooth: Disable clean turn on
    * This isn't needed anymore.

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -25,9 +25,6 @@
 // Wide-band speech support
 #define BTIF_HF_WBS_PREFERRED TRUE
 
-// skips conn update at conn completion
-#define BT_CLEAN_TURN_ON_DISABLED 1
-
 /* Disable enhanced sco commands */
 #define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
 
