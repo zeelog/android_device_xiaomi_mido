c528e144c8619a0378bc7efcb293e95acc70a7c2
ZeeLog <Danjel.Zammit@gmail.com>
2020-12-10 14:44:06 +0100

    mido: audio: remove inexistent package

--- device.mk
+++ device.mk
@@ -93,7 +93,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.2-impl \
-    android.hardware.soundtrigger@2.2-service \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.msm8953 \
