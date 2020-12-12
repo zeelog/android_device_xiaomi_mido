8f7edbec0977e909c4948166eca39a70b0b3be6b
ZeeLog <Danjel.Zammit@gmail.com>
2020-12-12 23:09:53 +0100

    mido: Import public.libraries from LA.UM.9.6.2.r1-03300-89xx.0

--- device.mk
+++ device.mk
@@ -309,6 +309,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
