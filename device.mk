9707189af9bc369cc075ee5a482077c7ff8b97f5
ZeeLog <Danjel.Zammit@gmail.com>
2020-12-19 23:34:15 +0100

    mido: Build android.hardware.health@2.1-impl.recovery package

--- device.mk
+++ device.mk
@@ -210,6 +210,7 @@ PRODUCT_COPY_FILES += \
 # Healthd
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl:64 \
+    android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
 # HIDL
