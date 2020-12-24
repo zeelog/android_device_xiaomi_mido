f857d7385e67f85321283f064711b73ec968ceb1
ZeeLog <Danjel.Zammit@gmail.com>
2020-12-24 21:32:57 +0100

    mido: Update & switch IMS Stack from LA.UM.9.6.2.r1-03300-89xx.0
    * Also build 64bit camera packages to satisfy dependencies.

--- device.mk
+++ device.mk
@@ -131,10 +131,15 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.common@1.0 \
+    android.hardware.camera.device@3.3:64 \
+    android.hardware.camera.device@3.4:64 \
+    android.hardware.camera.device@3.5:64 \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.common@1.0 \
+    android.hardware.camera.provider@2.5:64 \
+    android.hardware.camera.provider@2.6:64 \
     vendor.qti.hardware.camera.device@1.0 \
     camera.msm8953 \
     libmm-qcamera \
