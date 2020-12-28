7c553a98358fd3e5b3259fd974a794831264c053
Ethan Chen <intervigil@gmail.com>
2020-12-28 08:24:33 +0100

    mido: Remove unnecessary android.hardware.radio entries
    * These are implicitly included by AOSP.

--- device.mk
+++ device.mk
@@ -343,9 +343,6 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.2 \
-    android.hardware.radio@1.4 \
-    android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
     librmnetctl \
     libcnefeatureconfig \
