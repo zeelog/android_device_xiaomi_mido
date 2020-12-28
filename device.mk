52ee614ddc39b1f685152db044ba0cae262ffcac
Prakash Pabba <ppabba@codeaurora.org>
2020-12-28 08:18:27 +0100

    mido: Enable ipsec_tunnel feature
    Fixed : CTS issue due to ipsec_tunnel feature.
    
    CTS issues are showing failure on devices with first_api_level set to
    29 . With this api level certain test cases which weren't executing
    till now begin executing. Ex: android.net.cts.IpSecManagerTunnelTest.
    
    These tests mandatorily requrire ipsec_tunnel feature to be enabled.

--- device.mk
+++ device.mk
@@ -70,6 +70,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.print.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.print.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
