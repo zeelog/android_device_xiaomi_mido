68da91494d0fac556d8e5a5e786cbad1a2539ea9
Ethan Chen <intervigil@gmail.com>
2020-12-28 08:23:16 +0100

    mido: Remove unnecessary netutils packages
    * These are already packaged by AOSP.

--- device.mk
+++ device.mk
@@ -287,12 +287,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
-# Netutils
-PRODUCT_PACKAGES += \
-    netutils-wrapper-1.0 \
-    android.system.net.netd@1.0 \
-    libandroid_net
-
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
