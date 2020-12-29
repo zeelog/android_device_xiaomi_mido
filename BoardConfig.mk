b4fbabee104c566f22d187cbee098c28db8fe9c4
Felix <google@ix5.org>
2020-12-29 15:11:30 +0100

    mido: sepolicy: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,7 +159,7 @@ VENDOR_SECURITY_PATCH := 2020-09-05
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
