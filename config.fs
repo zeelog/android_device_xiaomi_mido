c5bd6a86514965812f205bf4ad0e06b176bf2aff
ZeeLog <Danjel.Zammit@gmail.com>
2021-01-01 20:53:33 +0100

    mido: GPS: Ditch slim_daemon
    * also, update sha1sum for modified izat.conf
      modified to disable slim_daemon process state.

--- config.fs
+++ config.fs
@@ -70,12 +70,6 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[vendor/bin/slim_daemon]
-mode: 0755
-user:  AID_GPS
-group: AID_GPS
-caps: NET_BIND_SERVICE
-
 [firmware/]	
 mode: 0771	
 user: AID_SYSTEM	
