bd9a0d91e06f475cbc69bf4a194288a1569bca71
Sujin Panicker <spanic@codeaurora.org>
2021-01-30 02:10:52 +0100

    mido: Add libnbaio to PRODUCT_PACKAGES
    libnbaio is not installed for system/lib/ for HY11
    builds.

--- device.mk
+++ device.mk
@@ -287,6 +287,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
+PRODUCT_PACKAGES += \
+    libnbaio
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
