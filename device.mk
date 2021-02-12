43939a5199972179c70ebfba9f0d9a47633cd6c2
ZeeLog <Danjel.Zammit@gmail.com>
2021-02-12 09:14:57 +0100

    mido: Build libavservices_minijail vendor variant

--- device.mk
+++ device.mk
@@ -289,6 +289,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
 PRODUCT_PACKAGES += \
+    libavservices_minijail.vendor \
     libnbaio
 
 # OMX
