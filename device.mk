b3a46f377fc08230a202c950597db633d0ac4f78
ZeeLog <Danjel.Zammit@gmail.com>
2021-02-12 22:13:43 +0100

    mido: Update perf blobs from LA.UM.9.6.2.r1-03600-89xx.0
    * Drop system side blobs
    * Build vendor.qti.hardware.perf@2.0  -2 from source
    * Add missing perf blobs

--- device.mk
+++ device.mk
@@ -313,7 +313,10 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    vendor.qti.hardware.perf@2.0.vendor \
+    vendor.qti.hardware.perf@2.1.vendor \
+    vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
