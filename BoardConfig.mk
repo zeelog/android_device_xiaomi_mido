e85472310516d35c8ab09796f37078fb1ff12e9c
ZeeLog <Danjel.Zammit@gmail.com>
2021-02-14 13:54:35 +0100

    mido: sepolicy: Update, cleanup & refactor
    * relabel fingerprint HAL
    * re-organize fingerprint rules
    * update gx_fpd rules for vndbinder access
    * other rules: minor cleanup & updates
    * suppress some denials
    * remove deprecated rules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -149,7 +149,6 @@ VENDOR_SECURITY_PATCH := 2020-11-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Treble
