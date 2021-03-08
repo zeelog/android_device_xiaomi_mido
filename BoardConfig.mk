864140f50125c2714fcb7c8948bf2d93e8b066c3
Georg Veichtlbauer <georg@vware.at>
2021-03-08 18:18:45 +0100

    mido: Remove BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED
    * It defaults to true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,7 +147,6 @@ include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Treble
-BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 BOARD_VNDK_VERSION := current
 
