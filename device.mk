10298c3efb33f3513bd0b2c505897f9ebfacfbd9
ZeeLog <Danjel.Zammit@gmail.com>
2021-03-27 16:33:03 +0100

    mido: Switch back to perf 2.1

--- device.mk
+++ device.mk
@@ -315,8 +315,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.0.vendor \
-    vendor.qti.hardware.perf@2.1.vendor \
-    vendor.qti.hardware.perf@2.2.vendor
+    vendor.qti.hardware.perf@2.1.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
