7fe42361eb5d2c8994449d5a7a8cc896a28baf69
tImIbreakdown <timifix@gmail.com>
2021-04-08 13:49:11 +0300

    mido: fakelogprint: resolve unused parameter warnings
    Change-Id: Ie252cefc64405ee98c423b7ae2a84797426c72e9

--- libshims/fakelogprint.cpp
+++ libshims/fakelogprint.cpp
@@ -1,6 +1,6 @@
 #include <log/log.h>
 
-int __android_log_print(int prio, const char* tag, const char* fmt, ...) {
+int __android_log_print(int /*prio*/, const char* /*tag*/, const char* /*fmt*/, ...) {
   // Goodix: please don't spam that much!
   return 0;
 }
