0294fde8d9932e42df29528ddc3d72150d3d8ae8
Mike Cailean <mcailean@codeaurora.org>
2021-04-11 22:44:35 +0200

    mido: config.fs: Add pd-mapper and xtwifi-client mapping
    Set the fs configuration for pd-mapper and xtwifi-client
    binaries.

--- config.fs
+++ config.fs
@@ -64,10 +64,22 @@ user:  AID_GPS
 group: AID_GPS
 caps: SETUID SETGID
 
+[vendor/bin/pd-mapper]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
 [vendor/bin/sensors.qti]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[vendor/bin/xtwifi-client]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
 caps: NET_BIND_SERVICE
 
 [firmware/]	
