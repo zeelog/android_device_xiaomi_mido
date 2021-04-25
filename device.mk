e4fdf37b9cd4434a10db133a9e429a905a60213c
ZeeLog <Danjel.Zammit@gmail.com>
2021-04-25 08:54:23 +0200

    mido: copy out 64bit variants only of btconfigstore

--- device.mk
+++ device.mk
@@ -130,8 +130,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor:64 \
+    vendor.qti.hardware.btconfigstore@2.0.vendor:64
 
 # Camera
 PRODUCT_PACKAGES += \
