8447c4d81ec99820cac134accf7193edde0bb02a
CrisBal <74150843+CBG-leeco@users.noreply.github.com>
2021-05-26 08:19:23 +0200

    mido: Build aidl power stats
    * Fix for aidl power stats errors.
    * New updated legacy-um sepolicy solved the android.hardware.power@1.0::IPower/default error
    - Now with this we fixing both errors:
    BatteryStatsService: Unable to load Power Hal or power.stats HAL
    hwservicemanager: getTransport: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.
    hwservicemanager: getTransport: Cannot find entry android.hardware.power@1.0::IPower/default in either framework or device manifest.

--- device.mk
+++ device.mk
@@ -317,6 +317,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock \
     vendor.qti.hardware.perf@2.0.vendor \
     vendor.qti.hardware.perf@2.1.vendor
 
