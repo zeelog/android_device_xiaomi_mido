10fb0fd135c816a1853a2f8ad995d7ead7163dd5
sb6596 <shubhamprince111@gmail.com>
2021-09-08 12:18:55 +0200

    mido: Downgrade SoundTrigger to 2.1

--- device.mk
+++ device.mk
@@ -90,7 +90,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.0-impl:32 \
-    android.hardware.soundtrigger@2.2-impl:32 \
+    android.hardware.soundtrigger@2.1-impl:32 \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.msm8953 \
