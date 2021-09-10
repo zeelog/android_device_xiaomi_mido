6ec3ad827bb88dc62f45b61d4ae5ab451f9b735d
Erfan Abdi <erfangplus@gmail.com>
2021-09-10 20:54:14 +0200

    mido: prepare to move to in-tree HALs

--- device.mk
+++ device.mk
@@ -37,6 +37,9 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Sna
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+    
+QCOM_SOONG_NAMESPACE := \
+    $(LOCAL_PATH)/qcom-caf
 
 # Permissions
 PRODUCT_COPY_FILES += \
