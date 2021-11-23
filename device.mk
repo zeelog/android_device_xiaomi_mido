92f8a9d2bec9095cbe3f863ed863b1f44a554409
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-11-23 16:39:35 +0100

    mido: Uprev bluetooth audio HIDL impl to 2.1
    * We get free LC3 support, isn't it poggers?

--- device.mk
+++ device.mk
@@ -91,7 +91,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.0-impl:32 \
+    android.hardware.bluetooth.audio@2.1-impl:32 \
     android.hardware.soundtrigger@2.1-impl:32 \
     audio.a2dp.default \
     audio.bluetooth.default \
