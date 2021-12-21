9ba9bb0736e49c981f88d31e18c74875505fa552
LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
2021-12-21 23:30:57 +0100

    mido: Only compile 32-bit audio.primary.msm8953
    * The audio stack is 32-bit so there is no need for a 64-bit audio.primary.msm8953.so lib to be built

--- device.mk
+++ device.mk
@@ -94,7 +94,7 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.1-impl:32 \
     audio.a2dp.default \
     audio.bluetooth.default \
-    audio.primary.msm8953 \
+    audio.primary.msm8953:32 \
     audio.r_submix.default \
     audio.usb.default \
     libaacwrapper \
