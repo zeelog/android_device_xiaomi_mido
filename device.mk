0e060f2c82cfecefe978f0796fa385518773c0e0
Roberto Sartori <roberto.sartori.android@gmail.com>
2022-01-19 12:40:20 +0100

    mido: disable compressed APEX
    Compressed APEX has been introduced in Android 12, and they are enabled
    by default inheriting updatable_apex.mk.
    Unfortunately, when the device is encrypted, they are causing bootloops
    on msm8953.
    As the only advantage is some space gain on disk, we can
    simply disable them: keep the variable immediatly after including the
    updatable_apex.mk to avoid being overwritten.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>
    Change-Id: I410bd4ad8d589971339dddab709c5b40d9c9a15c

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@
 $(call inherit-product, vendor/xiaomi/mido/mido-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
+# Apex
+PRODUCT_COMPRESSED_APEX := false
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
