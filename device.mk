af2a873a1d793689023759b748ad1563c2d62a5c
Alexander Koskovich <akoskovich@pm.me>
2022-02-05 22:31:47 +0100

    mido: Drop support for Snapdragon Camera.
     * Won't be implemented this cycle (or ever again hopefully).

--- device.mk
+++ device.mk
@@ -35,7 +35,6 @@ TARGET_SCREEN_WIDTH := 1080
 # RRO (Runtime Resource Overlay)
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Snap
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
@@ -154,8 +153,7 @@ PRODUCT_PACKAGES += \
     camera.msm8953 \
     libcamshim \
     libmm-qcamera \
-    libui_shim \
-    Snap
+    libui_shim
 
 # Component overrides
 PRODUCT_COPY_FILES += \
