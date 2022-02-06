2e150fee85de0b9681b31b421f3760b4f8853174
Danny Trunk <dtrunk90@gmail.com>
2022-02-06 11:50:02 +0100

    mido: uevent: Move ueventd.qcom.rc to /vendor/etc.
    This is the preferred location as of Android 12.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -80,7 +80,7 @@ LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 # Configuration scripts
