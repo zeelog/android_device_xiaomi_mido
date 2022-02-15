38a3ad2e99045b5b63ff986a55e1dd7063585706
ZeeLog <Danjel.Zammit@gmail.com>
2022-02-15 10:30:23 +0100

    mido: Cleanup ril packages & drop tel. injection
    * no longer needed

--- device.mk
+++ device.mk
@@ -373,11 +373,7 @@ PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
-    qti_telephony_utils.xml \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+    qti_telephony_utils.xml
 
 # Sensors
 PRODUCT_PACKAGES += \
