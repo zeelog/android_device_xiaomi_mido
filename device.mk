34d0f984498dc9095113965a02d5be67aca03fb3
ZeeLog <Danjel.Zammit@gmail.com>
2022-02-20 18:09:44 +0100

    mido: build gatekeeper & keymaster service vendor variant

--- device.mk
+++ device.mk
@@ -216,7 +216,8 @@ PRODUCT_PACKAGES += \
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl:64 \
-    android.hardware.gatekeeper@1.0-service
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.gatekeeper@1.0-service.vendor
 
 # GPS
 PRODUCT_PACKAGES += \
@@ -271,7 +272,8 @@ PRODUCT_PACKAGES += \
 # Keymaster HAL
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl:64 \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@3.0-service \
+    android.hardware.keymaster@3.0-service.vendor
 
 # Lights
 PRODUCT_PACKAGES += \
