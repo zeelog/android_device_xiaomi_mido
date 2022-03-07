ee3e4869302238e38cfc46cbb4eb241b5a2d3816
ZeeLog <Danjel.Zammit@gmail.com>
2022-03-07 15:38:12 +0100

    mido: Update Perf stack from LA.UM.9.6.2.r1-04200-89xx.0

--- device.mk
+++ device.mk
@@ -332,8 +332,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock \
-    vendor.qti.hardware.perf@2.0.vendor \
-    vendor.qti.hardware.perf@2.1.vendor
+    vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
