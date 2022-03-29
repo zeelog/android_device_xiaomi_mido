d1e701c1bf0f31cebd9ef9cbe48503f5f878544f
Bruno Martins <bgcngm@gmail.com>
2022-03-29 16:01:07 +0200

    mido: Copy cgroups and task_profiles directly from system/core
    This ensures that the most up-to-date JSON files are shipped.

--- device.mk
+++ device.mk
@@ -162,8 +162,8 @@ PRODUCT_PACKAGES += \
 
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # Component overrides
 PRODUCT_COPY_FILES += \
