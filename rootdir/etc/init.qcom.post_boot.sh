e2bc1e0f0ff59f7e8c835a9c566876e4e3ecb027
ZeeLog <Danjel.Zammit@gmail.com>
2022-07-26 08:54:00 +0200

    mido: Tune zram & swappinness
    * bump zram to 2Gb
    * reduce swappinness agressiveness

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -551,14 +551,14 @@ function configure_zram_parameters() {
     # For 512MB Go device, size = 384MB, set same for Non-Go.
     # For 1GB Go device, size = 768MB, set same for Non-Go.
     # For 2GB Go device, size = 1536MB, set same for Non-Go.
-    # For >2GB Non-Go devices, size = 50% of RAM size. Limit the size to 4GB.
+    # For >2GB Non-Go devices, size = 2GB. Limit the size to 4GB.
 
     let RamSizeGB="( $MemTotal / 1048576 ) + 1"
     diskSizeUnit=M
     if [ $RamSizeGB -le 2 ]; then
         let zRamSizeMB="( $RamSizeGB * 1024 ) * 3 / 4"
     else
-        let zRamSizeMB="( $RamSizeGB * 1024 ) / 2"
+        let zRamSizeMB=2048
     fi
 
     # use MB avoid 32 bit overflow
@@ -776,8 +776,8 @@ else
         fi
     fi
 
-    # Set swappiness to 100 for all targets
-    echo 100 > /proc/sys/vm/swappiness
+    # Set swappiness to 80 for all targets
+    echo 80 > /proc/sys/vm/swappiness
 
     # Disable wsf for all targets beacause we are using efk.
     # wsf Range : 1..1000 So set to bare minimum value 1.
