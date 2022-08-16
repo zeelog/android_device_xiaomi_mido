9fbd7bcc4ea8e9125d306164c7723f0d20918362
Bruno Martins <bgcngm@gmail.com>
2022-08-16 23:16:55 +0200

    mido: Drop default DRM service
    This is not needed, since Widevine DRM and Clearkey DRM
    services are available.

--- device.mk
+++ device.mk
@@ -207,8 +207,6 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl:64 \
-    android.hardware.drm@1.0-service-lazy \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4-service.clearkey
 
