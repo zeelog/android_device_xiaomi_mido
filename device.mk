c3f1b5014991345429d5a7214f0966d164657348
ZeeLog <Danjel.Zammit@gmail.com>
2022-08-30 12:47:20 +0200

    mido: Restore livedisplay
    This reverts commit 3f5a19d85e37801c4572a5c834a98f10b15e2a92.

--- device.mk
+++ device.mk
@@ -295,6 +295,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_mido
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service.xiaomi_mido
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_performance_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_v1.xml \
