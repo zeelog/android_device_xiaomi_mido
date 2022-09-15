c626c5339bd1b266ebf48862cc7becce855a499b
Carter Hsu <carterhsu@google.com>
2022-09-15 09:10:37 +0200

    mido: Update Audio HIDL to 7.1
    Bug: 205235270 | AOSP
    Test: build pass
    Signed-off-by: Carter Hsu <carterhsu@google.com>
    Change-Id: Ic54e2787ef27022556430c6c1db346b1e1348e39 | AOSP

--- device.mk
+++ device.mk
@@ -95,7 +95,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.0-impl:32 \
+    android.hardware.audio@7.1-impl:32 \
     android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl:32 \
