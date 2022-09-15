c327ac851f158452b33e03544671e48b133cf4ec
Josh Wu <joshwu@google.com>
2022-09-15 09:06:45 +0200

    mido: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261 | AOSP
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7 | AOSP
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7 | AOSP

--- device.mk
+++ device.mk
@@ -98,7 +98,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl:32 \
     android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.1-impl:32 \
+    android.hardware.bluetooth.audio-impl:32 \
     android.hardware.soundtrigger@2.1-impl:32 \
     audio.bluetooth.default \
     audio.primary.msm8953:32 \
