080943ed9514da34a6e9b4cdc59e9d7235e2621d
ZeeLog <Danjel.Zammit@gmail.com>
2022-09-15 10:55:04 +0200

    mido: Update aptX from coral
    * no diff from 12, update build tag only
    * from google/coral/coral:13/TP1A.220905.004/8927612:user/release-keys
    * also, bump vendor SPL

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -148,7 +148,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_USES_OLD_MNC_FORMAT := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-02-05
+VENDOR_SECURITY_PATCH := 2022-09-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
