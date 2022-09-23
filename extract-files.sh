6a9ef81b6e38a13bc82ba3320c563871d99bad87
ZeeLog <Danjel.Zammit@gmail.com>
2022-09-23 15:54:40 +0200

    mido: Update lib-imsvideocodec shim dep

--- extract-files.sh
+++ extract-files.sh
@@ -101,6 +101,6 @@ sed -i -e '$a\\    capabilities NET_BIND_SERVICE' "${DEVICE_BLOB_ROOT}"/vendor/e
 "${PATCHELF}" --add-needed "fakelogprint.so" "${DEVICE_BLOB_ROOT}"/vendor/lib64/hw/gxfingerprint.default.so
 
 # IMS
-"${PATCHELF}" --add-needed "libims-shim.so" "${DEVICE_BLOB_ROOT}"/system_ext/lib64/lib-imsvideocodec.so
+"${PATCHELF}" --add-needed "libgui_shim.so" "${DEVICE_BLOB_ROOT}"/system_ext/lib64/lib-imsvideocodec.so
 
 "${MY_DIR}/setup-makefiles.sh"
