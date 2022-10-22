139d7d523e30446ff7cd7064b39769d561b74c79
ZeeLog <Danjel.Zammit@gmail.com>
2022-10-22 20:37:48 +0200

    mido: Remove PRODUCT_COMPRESSED_APEX := false

--- device.mk
+++ device.mk
@@ -20,9 +20,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 TARGET_BOARD_PLATFORM := msm8953
 TARGET_BOARD_SUFFIX := _64
 
-# Apex
-PRODUCT_COMPRESSED_APEX := false
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
