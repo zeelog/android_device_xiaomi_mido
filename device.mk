c518256e683a81c07ab390180dde96e6c3e34d27
Mathieu Chartier <mathieuc@google.com>
2022-11-04 07:57:13 +0100

    mido: Speed profile services and wifi-service to reduce RAM and storage

--- device.mk
+++ device.mk
@@ -375,6 +375,9 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
+    
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
 # Thermal
 PRODUCT_COPY_FILES += \
