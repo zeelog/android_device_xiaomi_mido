472f57087d2826fa993c7485fb759365a21f0ba3
zeelog <Danjel.Zammit@gmail.com>
2022-12-29 13:23:09 +0100

    mido: parts: Drop kcal display calibration"
    This reverts commit 6743fa5ba34e530c14895e6d61b48f4c845ef8bb.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -26,20 +26,16 @@ import android.provider.Settings;
 
 import androidx.preference.PreferenceManager;
 
-import java.lang.Math.*;
-
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
-import org.lineageos.settings.kcal.Utils;
 import org.lineageos.settings.preferences.FileUtils;
-import org.lineageos.settings.preferences.SecureSettingSwitchPreference;
 import org.lineageos.settings.soundcontrol.SoundControlSettings;
 import org.lineageos.settings.torch.TorchSettings;
 import org.lineageos.settings.vibration.VibratorStrengthPreference;
 import org.lineageos.settings.vibration.VibratorSettings;
 import org.lineageos.settings.vibration.VibratorOverrideModeSwitch;
 
-public class BootCompletedReceiver extends BroadcastReceiver implements Utils {
+public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
@@ -60,33 +56,6 @@ public class BootCompletedReceiver extends BroadcastReceiver implements Utils {
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         enabled = sharedPrefs.getBoolean(VibratorSettings.PREF_VMAX_OVERRIDE_SWITCH, false);
         restore(VibratorOverrideModeSwitch.getFile(), enabled);
-
-        // KCAL
-        if (Settings.Secure.getInt(context.getContentResolver(), PREF_ENABLED, 0) == 1) {
-            FileUtils.setValue(KCAL_ENABLE, Settings.Secure.getInt(context.getContentResolver(),
-                    PREF_ENABLED, 0));
-
-            String rgbValue = Settings.Secure.getInt(context.getContentResolver(),
-                    PREF_RED, RED_DEFAULT) + " " +
-                    Settings.Secure.getInt(context.getContentResolver(), PREF_GREEN,
-                            GREEN_DEFAULT) + " " +
-                    Settings.Secure.getInt(context.getContentResolver(), PREF_BLUE,
-                            BLUE_DEFAULT);
-
-            FileUtils.setValue(KCAL_RGB, rgbValue);
-            FileUtils.setValue(KCAL_MIN, Settings.Secure.getInt(context.getContentResolver(),
-                    PREF_MINIMUM, MINIMUM_DEFAULT));
-            FileUtils.setValue(KCAL_SAT, Settings.Secure.getInt(context.getContentResolver(),
-                    PREF_GRAYSCALE, 0) == 1 ? 128 :
-                    Settings.Secure.getInt(context.getContentResolver(),
-                            PREF_SATURATION, SATURATION_DEFAULT) + SATURATION_OFFSET);
-            FileUtils.setValue(KCAL_VAL, Settings.Secure.getInt(context.getContentResolver(),
-                    PREF_VALUE, VALUE_DEFAULT) + VALUE_OFFSET);
-            FileUtils.setValue(KCAL_CONT, Settings.Secure.getInt(context.getContentResolver(),
-                    PREF_CONTRAST, CONTRAST_DEFAULT) + CONTRAST_OFFSET);
-            FileUtils.setValue(KCAL_HUE, Settings.Secure.getInt(context.getContentResolver(),
-                    PREF_HUE, HUE_DEFAULT));
-        }
 
         if (DozeUtils.isDozeEnabled(context) && DozeUtils.sensorsEnabled(context)) {
             if (DEBUG) Log.d(TAG, "Starting Doze service");
