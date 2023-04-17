4a25c9f43cbc169536af9259405c414599af4a75
zeelog <Danjel.Zammit@gmail.com>
2023-04-17 12:23:43 +0200

    mido: Drop max_comp_streams entry
    * ref: https://github.com/zeelog/android_kernel_xiaomi_mido/commit/43209ea2d17aae1540d4e28274e36404f72702f2

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -571,7 +571,6 @@ function configure_zram_parameters() {
 
     # Setup zram options
     echo lz4 > /sys/block/zram0/comp_algorithm
-    echo 8   > /sys/block/zram0/max_comp_streams
     echo 0   > /proc/sys/vm/page-cluster
 
     if [ -f /sys/block/zram0/disksize ]; then
