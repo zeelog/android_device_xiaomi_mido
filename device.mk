bb6f13e59b0666f7ff512fdd4b057317ba5e4c2d
ZeeLog <Danjel.Zammit@gmail.com>
2023-08-19 22:45:43 +0200

    mido: Unset bitness for DRM

--- device.mk
+++ device.mk
@@ -198,7 +198,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
-    android.hardware.drm@1.4.vendor:64
+    android.hardware.drm@1.4.vendor
 
 PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/configs/gpfspath_oem_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gpfspath_oem_config.xml
