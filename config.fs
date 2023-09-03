cfa95953a86c31b4e046009a34913420203e68c0
ZeeLog <Danjel.Zammit@gmail.com>
2023-09-03 20:23:54 +0200

    mido: adding xtwifi-client to config.fs

--- config.fs
+++ config.fs
@@ -79,6 +79,12 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
+[vendor/bin/xtwifi-client]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
 [firmware/]	
 mode: 0771	
 user: AID_SYSTEM	
