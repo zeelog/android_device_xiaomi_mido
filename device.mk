67c284cfe7ee7dccc2d0a73c4233ccdcdde08035
ZeeLog <Danjel.Zammit@gmail.com>
2023-10-01 21:18:51 +0200

    mido: thermal-hal: Rebrand to xiaomi_mido

--- device.mk
+++ device.mk
@@ -409,6 +409,9 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti.xiaomi_mido
+
 # Touch features
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.xiaomi_mido
