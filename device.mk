09a6b0a11efa8c0454c9c02b4af8ddc5dec88ad3
me-cafebabe <me.cafebabe@gmail.com>
2023-10-01 22:44:06 +0200

    mido: Switch to QTI Health AIDL

--- device.mk
+++ device.mk
@@ -246,11 +246,10 @@ PRODUCT_PACKAGES += \
     libloc_core \
     libgnss
 
-# Healthd
+# Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl:64 \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery
 
 # HIDL
 PRODUCT_PACKAGES += \
