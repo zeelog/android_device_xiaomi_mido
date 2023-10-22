42a2232763d6d1a6ee298ceca24ae70e61e47eca
AmeChanRain <downloadbot007@gmail.com>
2023-10-22 15:40:35 +0800

    mido: gps: Fix misleading indentation
    * That makes clang17 happy
    * Fix:
      device/xiaomi/mido/gps/android/utils/battery_listener.cpp:191:13: error: misleading indentation; statement is not part of the previous 'if' [-Werror,-Wmisleading-indentation]
                auto r = mHealth->unlinkToDeath(this);
                ^
    device/xiaomi/mido/gps/android/utils/battery_listener.cpp:189:9: note: previous statement is here
            if (mHealth != NULL)
            ^
    1 warning and 1 error generated.
    
    Change-Id: I3be1e3de45ce184c9d3dbcede57a312375a91498

--- gps/android/utils/battery_listener.cpp
+++ gps/android/utils/battery_listener.cpp
@@ -399,13 +399,14 @@ HidlBatteryListenerImpl::~HidlBatteryListenerImpl()
 {
     {
         std::lock_guard<std::mutex> _l(mLock);
-        if (mHealth != NULL)
+        if (mHealth != NULL) {
             mHealth->unregisterCallback(this);
             auto r = mHealth->unlinkToDeath(this);
             if (!r.isOk() || r == false) {
                 LOC_LOGe("Transaction error in unregister to HealthHAL death: %s",
                         r.description().c_str());
             }
+        }
     }
     mDone = true;
     mThread->join();
