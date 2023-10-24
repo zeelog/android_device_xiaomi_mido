93e0bb2a5ebbb580643213455e6b35cc79686af9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-10-24 21:41:38 +0200

    mido: Switch to two-stage init mounting
    * Stop handling vendor mounting from kernel, first stage init is gonna
      do that, together with system (which is now /system)
    * Use correct mount flags coming from kernel dts
    * Move system and vendor mount points to /dev/block/by-name/, we don't
      have bootdevice symlink to that point in init
    * Copy fstab to ramdisk for first stage init

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,7 +7,17 @@ LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_REQUIRED_MODULES := fstab.qcom_ramdisk
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom_ramdisk
+LOCAL_MODULE_STEM  := fstab.qcom
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
