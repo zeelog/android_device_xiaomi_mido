ce79f28c3da5ad386e8341f5c211fb8aea849041
ZeeLog <Danjel.Zammit@gmail.com>
2023-11-27 21:25:18 +0100

    mido: Move uclamp tunings to separate rc

--- device.mk
+++ device.mk
@@ -377,6 +377,7 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.usb.rc \
     init.parts.rc \
     init.target.rc \
+    init.uclamp.rc \
     ueventd.qcom.rc
 
 # RIL
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -59,6 +59,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.uclamp.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.target.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.qcom.rc
 LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional
