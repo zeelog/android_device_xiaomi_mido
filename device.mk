2a4df5820b5122b973993c500b1db2b8331d8b56
ZeeLog <Danjel.Zammit@gmail.com>
2023-12-23 20:58:21 +0100

    mido: Nuke mock power stats hal

--- device.mk
+++ device.mk
@@ -339,7 +339,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock \
     vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
